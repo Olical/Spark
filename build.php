1cf9929b0daf7cd22e3a33cc3ce2c8924494b312
Oliver Caldwell <oliver@flowdev.co.uk>
2010-10-22 20:33:44 +0100

    Added header/content/footer variables

--- build.php
+++ build.php
@@ -5,6 +5,10 @@
  * @author Oliver Caldwell
  * @version 0.1
  */
+$header = 'function spark() {';
+$content = '';
+$footer = '} var spark = new spark();';
+
 if($handle = opendir('modules'))
 {
 	while(false !== ($module = readdir($handle)))

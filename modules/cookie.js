cb1944310561a264f8931c08963fed9ae3065405
Oliver Caldwell <oliver@flowdev.co.uk>
2010-10-23 18:36:54 +0100

    Finished cookie module, now returns false if not found

--- modules/cookie.js
+++ modules/cookie.js
@@ -2,46 +2,37 @@ function(name, content, duration) {
 	if(content === undefined)
 	{
 		// Return cookie's content
+		var nameEQ = name + '=';
 		var ca = document.cookie.split(';');
 		for(var i = 0; i < ca.length; i++)
 		{
 			var c = ca[i];
-			while(c.charAt(0)==' ')
+			while(c.charAt(0) == ' ')
 			{
-				c = c.substring(1,c.length)
+				c = c.substring(1, c.length);
 			}
 			if(c.indexOf(nameEQ) == 0)
 				return c.substring(nameEQ.length, c.length);
 		}
+		return false;
 	}
 	else
 	{
-		if(time === undefined)
+		if(duration === undefined)
 		{
 			// Create cookie with preset duration of one year
 			var date = new Date();
 			date.setTime(date.getTime() + 31536000000);
 			var expires = '; expires=' + date.toGMTString();
-			document.cookie = name + '=' + value + expires + '; path=/';
+			document.cookie = name + '=' + content + expires + '; path=/';
 		}
 		else
 		{
 			// Create cookie with specified duration
 			var date = new Date();
-			date.setTime(date.getTime() + duration * 1000);
+			date.setTime(date.getTime() + duration);
 			var expires = '; expires=' + date.toGMTString();
-			document.cookie = name + '=' + value + expires + '; path=/';
+			document.cookie = name + '=' + content + expires + '; path=/';
 		}
 	}
-	/*
-	function createCookie(name,value,days) {
-		if (days) {
-			var date = new Date();
-			date.setTime(date.getTime()+(days*24*60*60*1000));
-			var expires = "; expires="+date.toGMTString();
-		}
-		else var expires = "";
-		document.cookie = name+"="+value+expires+"; path=/";
-	}
-	*/
 }

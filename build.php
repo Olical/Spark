571c3175c4f7ca4b929f1bf60597dd3b84847275
Oliver Caldwell <oliver@flowdev.co.uk>
2010-10-23 10:49:30 +0100

    build.php reads config file and splits it into an array

--- build.php
+++ build.php
@@ -5,6 +5,21 @@
  * @author Oliver Caldwell
  * @version 0.1
  */
+
+// Set up the variables
 $header = 'function spark() {' . "\n";
 $content = '';
 $footer = '} ' . "\n" . 'var spark = new spark();';
+
+if(file_exists('modules.conf'))
+{
+	// Read the config file and split into an array
+	$modules = explode(';', file_get_contents('modules.conf'));
+	
+	// Remove last one
+	array_pop($modules);
+}
+else
+{
+	print('Can not find modules.conf');
+}

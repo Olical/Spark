4ae4a9aaa3cf0c452c85b0fe2ee86dee158e85e5
Oliver Caldwell <oliver@flowdev.co.uk>
2010-10-23 17:51:16 +0100

    Added the cookie module with if statements

---
+++ modules/cookie.js
@@ -0,0 +1,43 @@
+function(name, content, duration) {
+	if(content === undefined)
+	{
+		// Return cookie's content
+	}
+	else
+	{
+		if(time === undefined)
+		{
+			// Create cookie with preset duration
+		}
+		else
+		{
+			// Create cookie with specified duration
+		}
+	}
+	/*
+	function createCookie(name,value,days) {
+		if (days) {
+			var date = new Date();
+			date.setTime(date.getTime()+(days*24*60*60*1000));
+			var expires = "; expires="+date.toGMTString();
+		}
+		else var expires = "";
+		document.cookie = name+"="+value+expires+"; path=/";
+	}
+
+	function readCookie(name) {
+		var nameEQ = name + "=";
+		var ca = document.cookie.split(';');
+		for(var i=0;i < ca.length;i++) {
+			var c = ca[i];
+			while (c.charAt(0)==' ') c = c.substring(1,c.length);
+			if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+		}
+		return null;
+	}
+
+	function eraseCookie(name) {
+		createCookie(name,"",-1);
+	}
+	*/
+}

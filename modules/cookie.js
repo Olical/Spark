2b1aa0c265685ffa79d3649e1a11ce8f78f426f8
Oliver Caldwell <oliver@flowdev.co.uk>
2010-10-23 18:13:49 +0100

    Cookie function now can read / set cookies

--- modules/cookie.js
+++ modules/cookie.js
@@ -2,16 +2,35 @@ function(name, content, duration) {
 	if(content === undefined)
 	{
 		// Return cookie's content
+		var ca = document.cookie.split(';');
+		for(var i = 0; i < ca.length; i++)
+		{
+			var c = ca[i];
+			while(c.charAt(0)==' ')
+			{
+				c = c.substring(1,c.length)
+			}
+			if(c.indexOf(nameEQ) == 0)
+				return c.substring(nameEQ.length, c.length);
+		}
 	}
 	else
 	{
 		if(time === undefined)
 		{
-			// Create cookie with preset duration
+			// Create cookie with preset duration of one year
+			var date = new Date();
+			date.setTime(date.getTime() + 31536000000);
+			var expires = '; expires=' + date.toGMTString();
+			document.cookie = name + '=' + value + expires + '; path=/';
 		}
 		else
 		{
 			// Create cookie with specified duration
+			var date = new Date();
+			date.setTime(date.getTime() + duration * 1000);
+			var expires = '; expires=' + date.toGMTString();
+			document.cookie = name + '=' + value + expires + '; path=/';
 		}
 	}
 	/*
@@ -23,21 +42,6 @@ function(name, content, duration) {
 		}
 		else var expires = "";
 		document.cookie = name+"="+value+expires+"; path=/";
-	}
-
-	function readCookie(name) {
-		var nameEQ = name + "=";
-		var ca = document.cookie.split(';');
-		for(var i=0;i < ca.length;i++) {
-			var c = ca[i];
-			while (c.charAt(0)==' ') c = c.substring(1,c.length);
-			if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-		}
-		return null;
-	}
-
-	function eraseCookie(name) {
-		createCookie(name,"",-1);
 	}
 	*/
 }

8f471b749b32294d8fd586007b8c2be86c6f299f
Oliver Caldwell <oliver@flowdev.co.uk>
2010-10-23 10:54:13 +0100

    Lets the user know how many where found

--- build.php
+++ build.php
@@ -7,6 +7,7 @@
  */
 
 // Set up the variables
+print('Preparing document');
 $header = 'function spark() {' . "\n";
 $content = '';
 $footer = '} ' . "\n" . 'var spark = new spark();';
@@ -14,10 +15,18 @@ $footer = '} ' . "\n" . 'var spark = new spark();';
 if(file_exists('modules.conf'))
 {
 	// Read the config file and split into an array
+	print('Reading and parsing file');
 	$modules = explode(';', file_get_contents('modules.conf'));
 	
 	// Remove last one
 	array_pop($modules);
+	
+	print(count($modules) . ' modules specified.');
+	
+	foreach($modules as $module)
+	{
+		
+	}
 }
 else
 {

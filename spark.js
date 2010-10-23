32f9dee17cc66776a602959ee9a73d1832ebf3ec
Oliver Caldwell <oliver@flowdev.co.uk>
2010-10-23 18:37:12 +0100

    Built with cookie module

--- spark.js
+++ spark.js
@@ -5,14 +5,15 @@ else
 {element.textContent=content;}
 else if(append===true)
 {element.textContent+=content;}}};this.cookie=function(name,content,duration){if(content===undefined)
-{var ca=document.cookie.split(';');for(var i=0;i<ca.length;i++)
+{var nameEQ=name+'=';var ca=document.cookie.split(';');for(var i=0;i<ca.length;i++)
 {var c=ca[i];while(c.charAt(0)==' ')
-{c=c.substring(1,c.length)}
+{c=c.substring(1,c.length);}
 if(c.indexOf(nameEQ)==0)
-return c.substring(nameEQ.length,c.length);}}
+return c.substring(nameEQ.length,c.length);}
+return false;}
 else
-{if(time===undefined)
-{var date=new Date();date.setTime(date.getTime()+31536000000);var expires='; expires='+date.toGMTString();document.cookie=name+'='+value+expires+'; path=/';}
+{if(duration===undefined)
+{var date=new Date();date.setTime(date.getTime()+31536000000);var expires='; expires='+date.toGMTString();document.cookie=name+'='+content+expires+'; path=/';}
 else
-{var date=new Date();date.setTime(date.getTime()+duration*1000);var expires='; expires='+date.toGMTString();document.cookie=name+'='+value+expires+'; path=/';}}}}
+{var date=new Date();date.setTime(date.getTime()+duration);var expires='; expires='+date.toGMTString();document.cookie=name+'='+content+expires+'; path=/';}}}}
 var s=new Spark()

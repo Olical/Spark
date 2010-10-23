b75a1d1742ea63918e4cf2aa54b89dc402a3f6c9
Oliver Caldwell <oliver@flowdev.co.uk>
2010-10-23 18:14:27 +0100

    Rebuilt spark.js with cookies included

--- spark.js
+++ spark.js
@@ -4,5 +4,15 @@ else
 {if(append===undefined||append===false)
 {element.textContent=content;}
 else if(append===true)
-{element.textContent+=content;}}};}
+{element.textContent+=content;}}};this.cookie=function(name,content,duration){if(content===undefined)
+{var ca=document.cookie.split(';');for(var i=0;i<ca.length;i++)
+{var c=ca[i];while(c.charAt(0)==' ')
+{c=c.substring(1,c.length)}
+if(c.indexOf(nameEQ)==0)
+return c.substring(nameEQ.length,c.length);}}
+else
+{if(time===undefined)
+{var date=new Date();date.setTime(date.getTime()+31536000000);var expires='; expires='+date.toGMTString();document.cookie=name+'='+value+expires+'; path=/';}
+else
+{var date=new Date();date.setTime(date.getTime()+duration*1000);var expires='; expires='+date.toGMTString();document.cookie=name+'='+value+expires+'; path=/';}}}}
 var s=new Spark()

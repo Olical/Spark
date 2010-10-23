314a98c12b6199ac9f2fd8ec400de43a637f0435
Oliver Caldwell <oliver@flowdev.co.uk>
2010-10-23 15:08:06 +0100

    Can now change/append content of elements

--- modules/content.js
+++ modules/content.js
@@ -1,19 +1,22 @@
 function(element, content, append) {
+	var element = document.querySelectorAll(element)[0];
+	
 	if(content === undefined)
 	{
 		// Return content of the selected element
-		var element = document.querySelectorAll(element);
-		return element[0].textContent;
+		return element.textContent;
 	}
 	else
 	{
 		if(append === undefined || append === false)
 		{
 			// Replace content
+			element.textContent = content;
 		}
 		else if(append === true)
 		{
 			// Append content
+			element.textContent += content;
 		}
 	}
 };
--- spark.js
+++ spark.js
@@ -1,8 +1,8 @@
-function Spark(){this.content=function(element,content,append){if(content===undefined)
-{var element=document.querySelectorAll(element);return element[0].textContent;}
+function Spark(){this.content=function(element,content,append){var element=document.querySelectorAll(element)[0];if(content===undefined)
+{return element.textContent;}
 else
 {if(append===undefined||append===false)
-{}
+{element.textContent=content;}
 else if(append===true)
-{}}};}
+{element.textContent+=content;}}};}
 var s=new Spark()

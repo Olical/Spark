1b4b98306c2d8660dc878c063fa772d28bd5ee3a
Oliver Caldwell <oliver@flowdev.co.uk>
2010-10-25 11:15:12 +0100

    Removed licence and remove last

--- build.php
+++ build.php
@@ -12,30 +12,7 @@ include('jsmin.php');
 
 // Set up the variables
 print('Preparing document.' . "\n");
-$header = <<<_END
-/*!
- * Copyright (c) 2010 Oliver Caldwell
- * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-function Spark() {
-_END;
+$header = 'function Spark() {' . "\n";
 $content = '';
 $footer = '} '. "\n" . 'var s = new Spark()';
 
@@ -44,9 +21,6 @@ if(file_exists('modules.conf'))
 	// Read the config file and split into an array
 	print('Reading and parsing file.' . "\n");
 	$modules = explode("\n", file_get_contents('modules.conf'));
-	
-	// Remove last
-	array_pop($modules);
 	
 	// Count modules
 	print(count($modules) . ' modules specified.' . "\n");

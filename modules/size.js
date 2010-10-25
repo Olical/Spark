286c4e9b71d9b00674e0c3d120b03bd2ed6c4564
Oliver Caldwell <oliver@flowdev.co.uk>
2010-10-25 14:18:14 +0100

    Added basic if statements to size module

--- modules/size.js
+++ modules/size.js
@@ -1,3 +1,18 @@
 function(element, width, height, timeframe) {
 	element = document.querySelectorAll(element)[0];
+	if(width !== undefined && height !== undefined)
+	{
+		if(timeframe !== undefined)
+		{
+			// Resize in timeframe
+		}
+		else
+		{
+			// Resize
+		}
+	}
+	else
+	{
+		// Give size as an object.
+	}
 };

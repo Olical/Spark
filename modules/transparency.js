dfa6e2961eddc963eeb96331ae3ccf53a227517b
Oliver Caldwell <oliver@flowdev.co.uk>
2010-10-26 22:06:33 +0100

    Added basic if structure for the transparency module

--- modules/transparency.js
+++ modules/transparency.js
@@ -1,3 +1,19 @@
 function(element, transparency, timeframe) {
+	element = document.querySelectorAll(element)[0];
 	
+	if(transparency === undefined)
+	{
+		// Return the transparency of the element
+	}
+	else
+	{
+		if(timeframe === undefined)
+		{
+			// Change transparency instantly
+		}
+		else
+		{
+			// Change transparency over the timeframe
+		}
+	}
 };

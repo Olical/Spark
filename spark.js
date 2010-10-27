b0427235b065ad55faea233f3d120715c6e52b93
Oliver Caldwell <oliver@flowdev.co.uk>
2010-10-27 20:38:48 +0100

    Built Spark with the passing of objects allowed

--- spark.js
+++ spark.js
@@ -1,4 +1,6 @@
-function Spark(){this.content=function(element,content,append){element=document.querySelectorAll(element)[0];if(content===undefined)
+function Spark(){this.content=function(element,content,append){if(isNaN(element))
+{element=document.querySelectorAll(element)[0];}
+if(content===undefined)
 {return element.innerHTML;}
 else
 {if(append===undefined||append===false)
@@ -15,7 +17,13 @@ else
 {if(duration===undefined)
 {var date=new Date();date.setTime(date.getTime()+31536000000);var expires='; expires='+date.toGMTString();document.cookie=name+'='+content+expires+'; path=/';}
 else
-{var date=new Date();date.setTime(date.getTime()+duration);var expires='; expires='+date.toGMTString();document.cookie=name+'='+content+expires+'; path=/';}}};this.click=function(element,callback){element=document.querySelectorAll(element)[0];element.addEventListener('click',callback,false);};this.mouse=function(element,action,callback){element=document.querySelectorAll(element)[0];element.addEventListener('mouse'+action,callback,false);};this.size=function(element,width,height,timeframe,callback){var rawelement=element;element=document.querySelectorAll(element)[0];if(width!==undefined&&height!==undefined)
+{var date=new Date();date.setTime(date.getTime()+duration);var expires='; expires='+date.toGMTString();document.cookie=name+'='+content+expires+'; path=/';}}};this.click=function(element,callback){if(isNaN(element))
+{element=document.querySelectorAll(element)[0];}
+element.addEventListener('click',callback,false);};this.mouse=function(element,action,callback){if(isNaN(element))
+{element=document.querySelectorAll(element)[0];}
+element.addEventListener('mouse'+action,callback,false);};this.size=function(element,width,height,timeframe,callback){var rawelement=element;if(isNaN(element))
+{element=document.querySelectorAll(element)[0];}
+if(width!==undefined&&height!==undefined)
 {if(timeframe!==undefined)
 {var widthdiff=width-parseInt(element.style.width);var heightdiff=height-parseInt(element.style.height);var steptime=timeframe/100;var widthpps=widthdiff/100;var heightpps=heightdiff/100;var origwidth=parseInt(element.style.width);var origheight=parseInt(element.style.height);var timers=[];for(var i=0;i<=100;i++)
 {timers[i]=setTimeout((function(privateEye){return function(){element.style.width=origwidth+(widthpps*privateEye)+'px';element.style.height=origheight+(heightpps*privateEye)+'px';}})(i),i*steptime);}
@@ -24,13 +32,17 @@ if(callback!==undefined)
 else
 {element.style.width=width;element.style.height=height;}}
 else
-{function size(){var width;var height;};size.width=element.offsetWidth;size.height=element.offsetHeight;return size;}};this.value=function(element,content,append){element=document.querySelectorAll(element)[0];if(content===undefined)
+{function size(){var width;var height;};size.width=element.offsetWidth;size.height=element.offsetHeight;return size;}};this.value=function(element,content,append){if(isNaN(element))
+{element=document.querySelectorAll(element)[0];}
+if(content===undefined)
 {return element.value;}
 else
 {if(append===undefined||append===false)
 {element.value=content;}
 else if(append===true)
-{element.value+=content;}}};this.visible=function(element,visible){element=document.querySelectorAll(element)[0];if(visible===true)
+{element.value+=content;}}};this.visible=function(element,visible){if(isNaN(element))
+{element=document.querySelectorAll(element)[0];}
+if(visible===true)
 {element.style.display='block';}
 else if(visible===false)
 {element.style.display='none';}
@@ -38,7 +50,9 @@ else if(visible===undefined)
 {if(element.style!='none')
 {element.style.display='none';}
 else
-{element.style.display='block';}}};this.opacity=function(element,opacity,timeframe,callback){element=document.querySelectorAll(element)[0];if(opacity===undefined)
+{element.style.display='block';}}};this.opacity=function(element,opacity,timeframe,callback){if(isNaN(element))
+{element=document.querySelectorAll(element)[0];}
+if(opacity===undefined)
 {return element.style.opacity*100;}
 else
 {if(timeframe===undefined)

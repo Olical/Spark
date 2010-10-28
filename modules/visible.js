3f54c410ac234b590598293d0c283d065df5d1b5
Oliver Caldwell <oliver@flowdev.co.uk>
2010-10-28 20:04:20 +0100

    Made the visible module smaller

--- modules/visible.js
+++ modules/visible.js
@@ -1,30 +1,16 @@
 function(element, visible) {
 	if(isNaN(element))
-	{
 		element = document.querySelectorAll(element)[0];
-	}
 	if(visible === true)
-	{
-		// Show it
-		element.style.display = 'block';
-	}
+		element.style.display = 'block'; // Show it
 	else if(visible === false)
-	{
-		// Hide it
-		element.style.display = 'none';
-	}
+		element.style.display = 'none'; // Hide it
 	else if(visible === undefined)
 	{
 		// Toggle
 		if(element.style.display != 'none')
-		{
-			// Hide it
-			element.style.display = 'none';
-		}
+			element.style.display = 'none'; // Hide it
 		else
-		{
-			// Show it
-			element.style.display = 'block';
-		}
+			element.style.display = 'block'; // Show it
 	}
 };

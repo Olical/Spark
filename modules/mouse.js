427bf4c530cf0124fa1532e8e07e0f96100e51ba
Oliver Caldwell <oliver@flowdev.co.uk>
2010-10-28 19:59:04 +0100

    Made mouse module a bit smaller

--- modules/mouse.js
+++ modules/mouse.js
@@ -1,7 +1,5 @@
 function(element, action, callback) {
 	if(isNaN(element))
-	{
 		element = document.querySelectorAll(element)[0];
-	}
 	element.addEventListener('mouse' + action, callback, false);
 };

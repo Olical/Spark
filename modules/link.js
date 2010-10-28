1148a372f941a5de44baa51c70bcab6b3bca1f1e
Oliver Caldwell <oliver@flowdev.co.uk>
2010-10-28 15:14:24 +0100

    Finished the link module

--- modules/link.js
+++ modules/link.js
@@ -1,6 +1,5 @@
-function(element, find, href) {
+function(element, find, link) {
 	if(isNaN(element))
-	{
 		element = document.querySelectorAll(element)[0];
-	}
+	element.innerHTML = element.innerHTML.replace(new RegExp('(' + find + ')', 'gi'), "<a href='" + link + "'>$1</a>");
 };

0d4ace596c6e73ec34f88f9e8ff479d76fb42911
Oliver Caldwell <oliver@flowdev.co.uk>
2010-10-29 11:16:21 +0100

    Mouse and click module now pass the event to the callback

--- modules/click.js
+++ modules/click.js
@@ -1,5 +1,5 @@
 function(element, callback) {
 	if(isNaN(element))
 		element = document.querySelectorAll(element)[0];
-	element.addEventListener('click', callback, false);
+	element.addEventListener('click', function(event) { callback(event); }, false);
 };
--- modules/mouse.js
+++ modules/mouse.js
@@ -1,5 +1,5 @@
 function(element, action, callback) {
 	if(isNaN(element))
 		element = document.querySelectorAll(element)[0];
-	element.addEventListener('mouse' + action, callback, false);
+	element.addEventListener('mouse' + action, function(event) { callback(event); }, false);
 };

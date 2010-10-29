e8f684b567d8448cbd0f95bed4e3b6edcc944ad8
Oliver Caldwell <oliver@flowdev.co.uk>
2010-10-29 14:03:21 +0100

    Built with the new round module, still working on it though, it rounds corners

--- spark.js
+++ spark.js
@@ -64,5 +64,7 @@ file+='?'+data;xmlhttp.open(method,file,false);if(method=='POST')
 xmlhttp.send(data);else
 xmlhttp.send(null);}
 else
-xmlhttp.send(null);return xmlhttp.responseText;};}
+xmlhttp.send(null);return xmlhttp.responseText;};this.round=function(element){var raw=element;if(isNaN(element))
+element=document.querySelectorAll(element)[0];element.style.position='relative';element.innerHTML='<div class=\'spark-corner-top\' style=\'width: 100%; height: 5px;\'><div class=\'corner1\'></div><div class=\'corner2\'></div><div class=\'corner3\'></div><div class=\'corner4\'></div><div class=\'corner5\'></div></div>'+element.innerHTML;element.innerHTML+='<div class=\'spark-corner-bottom\' style=\'width: 100%; height: 5px;\'><div class=\'corner1\'></div><div class=\'corner2\'></div><div class=\'corner3\'></div><div class=\'corner4\'></div><div class=\'corner5\'></div></div>';var topBox=document.querySelectorAll(raw+' div.spark-corner-top')[0];var bottomBox=document.querySelectorAll(raw+' div.spark-corner-bottom')[0];var top=document.querySelectorAll(raw+' div.spark-corner-top div');var bottom=document.querySelectorAll(raw+' div.spark-corner-bottom div');topBox.style.width='100%';topBox.style.height='5px';topBox.style.position='absolute';topBox.style.top='-5px';bottomBox.style.width='100%';bottomBox.style.height='5px';bottomBox.style.position='absolute';bottomBox.style.bottom='-5px';for(var i=0;i<5;i++)
+{top[i].style.height='1px';top[i].style.width=(element.offsetWidth-5)+(i+1)+'px';top[i].style.backgroundColor=element.style.backgroundColor;bottom[i].style.height='1px';bottom[i].style.width=element.offsetWidth-(i+1)+'px';bottom[i].style.backgroundColor=element.style.backgroundColor;}};}
 var s=new Spark()

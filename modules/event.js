f202214e158d0bb8b45565326f693792f29bff9c
Oliver Caldwell <oliver@flowdev.co.uk>
2010-10-30 13:21:54 +0100

    Created event module

---
+++ modules/event.js
@@ -0,0 +1,13 @@
+function(element, type, callback) {
+	if(isNaN(element))
+		element = document.querySelector(element);
+	
+	if(element.addEventListener)
+		element.addEventListener(type, function(event) { callback(event); }, false);
+	else if(element.attachEvent)
+	{
+		element['e' + type + callback] = callback;
+		element[type + callback] = function() { element['e' + type + callback](window.event); }
+		element.attachEvent('on' + type, element[type + callback]);
+	}
+};

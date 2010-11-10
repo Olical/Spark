338b08b73b8e536f72eac3ac6b4eba811d06519a
Oliver Caldwell <oliver@flowdev.co.uk>
2010-11-10 20:47:46 +0000

    Made the focus module slightly smaller, less braces and added the object to array check.

--- modules/focus.js
+++ modules/focus.js
@@ -1,8 +1,9 @@
 function(element) {
 	if(typeof element == 'string')
 		element = Sizzle(element);
+	else if(element.constructor != Array)
+		element = new Array(element);
+	
 	for(var e in element)
-	{
 		element[e].focus();
-	}
 };

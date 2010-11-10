9b10c75e1034bb5ed263990f51a5d753e53c644a
Oliver Caldwell <oliver@flowdev.co.uk>
2010-11-10 20:25:26 +0000

    Did the object to array check on content module

--- modules/content.js
+++ modules/content.js
@@ -1,6 +1,9 @@
 function(element, content, append) {
 	if(typeof element == 'string')
 		element = Sizzle(element);
+	else if(element.constructor != Array)
+		element = new Array(element);
+	
 	for(var e in element)
 	{
 		if(content === undefined)

a95bd50415ab0aba8cdcb83cf820ae9f7fdb502c
Oliver Caldwell <oliver@flowdev.co.uk>
2010-11-10 20:56:14 +0000

    Allowed objects in the link module and took out some curly braces

--- modules/link.js
+++ modules/link.js
@@ -1,8 +1,9 @@
 function(element, find, link) {
 	if(typeof element == 'string')
 		element = Sizzle(element);
+	else if(element.constructor != Array)
+		element = new Array(element);
+	
 	for(var e in element)
-	{
 		element[e].innerHTML = element[e].innerHTML.replace(new RegExp('(' + find + ')', 'gi'), "<a href='" + link + "'>$1</a>");
-	}
 };

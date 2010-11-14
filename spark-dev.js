c8ef4e3ecef6c438259cacdacc4c6feb958acfb2
Oliver Caldwell <oliver@flowdev.co.uk>
2010-11-14 19:02:47 +0000

    Added the cookie function and changed the no bracket fix to be...well...awesome.

--- spark-dev.js
+++ spark-dev.js
@@ -1403,6 +1403,44 @@ window.Spark = window.$ = function(selector, context) {
 				xmlhttp.send(null); // Otherwise just send the data
 			
 			return xmlhttp.responseText;
+		},
+		cookie: function(name, content, duration) {
+			if(content === undefined)
+			{
+				// Return cookie's content
+				var nameEQ = name + '=';
+				var ca = document.cookie.split(';');
+				for(var i = 0; i < ca.length; i++)
+				{
+					var c = ca[i];
+					while(c.charAt(0) == ' ')
+					{
+						c = c.substring(1, c.length);
+					}
+					if(c.indexOf(nameEQ) == 0)
+						return c.substring(nameEQ.length, c.length);
+				}
+				return false;
+			}
+			else
+			{
+				if(duration === undefined)
+				{
+					// Create cookie with preset duration of one year
+					var date = new Date();
+					date.setTime(date.getTime() + 31536000000);
+					var expires = '; expires=' + date.toGMTString();
+					document.cookie = name + '=' + content + expires + '; path=/';
+				}
+				else
+				{
+					// Create cookie with specified duration
+					var date = new Date();
+					date.setTime(date.getTime() + duration);
+					var expires = '; expires=' + date.toGMTString();
+					document.cookie = name + '=' + content + expires + '; path=/';
+				}
+			}
 		}
 	};
 	
@@ -1411,5 +1449,7 @@ window.Spark = window.$ = function(selector, context) {
 };
 
 // Take out the need for brackets on functions that do not need an element
-window.Spark.ajax = window.$.ajax = window.Spark().ajax;
+fn = {'ajax', 'cookie'};
+for(i in fn)
+	window.Spark[fn] = window.$[fn] = window.Spark()[fn];
 })();

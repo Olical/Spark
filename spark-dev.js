70a2ca9f15300b995598ae04deae845f7415278a
Oliver Caldwell <oliver@flowdev.co.uk>
2010-11-18 21:25:34 +0000

    Added asyncronous ajax

--- spark-dev.js
+++ spark-dev.js
@@ -1390,22 +1390,29 @@ window.Spark = window.$ = function(selector, context) {
 			if(window.XMLHttpRequest)
 				xmlhttp = new XMLHttpRequest(); // For IE7+, Firefox, Chrome, Opera, Safari
 			else
-				xmlhttp = new ActiveXObject('Microsoft.XMLHTTP'); // For IE5 and IE6
+				xmlhttp = new ActiveXObject('Microsoft.XMLHTTP'); // For IE6, IE5
 			
 			// Convert to upper case.
 			method = method.toUpperCase();
 			
 			// If the method is get then append the data to the file string
-			if(method == 'GET' && data !== undefined)
+			if(method == 'GET' && data !== undefined && data !== false)
 				file += '?' + data;
 			
-			xmlhttp.open(method, file, false);
+			if(callback != undefined)
+				xmlhttp.onreadystatechange = function() {
+					if(xmlhttp.readyState == 4) {
+						callback(xmlhttp.responseText);
+					}
+				};
+			
+			xmlhttp.open(method, file, (callback != undefined) ? true : false);
 			
 			// If the method is post then send the headers and the data
 			if(method == 'POST')
 			{
 				xmlhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-				if(data !== undefined)
+				if(data !== undefined && date !== false)
 					xmlhttp.send(data);
 				else
 					xmlhttp.send(null);
@@ -1413,7 +1420,10 @@ window.Spark = window.$ = function(selector, context) {
 			else
 				xmlhttp.send(null); // Otherwise just send the data
 			
-			return xmlhttp.responseText;
+			window.xmlhttp = xmlhttp;
+			
+			if(callback == undefined)
+				return xmlhttp.responseText;
 		},
 		cookie: function(name, content, duration) {
 			if(content === undefined)

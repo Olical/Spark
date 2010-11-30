a2f90d9b273465dbe83cda15b2a2176b0242379d
Oliver Caldwell <oliver@flowdev.co.uk>
2010-11-30 19:20:01 +0000

    Added basic structure to the replace plugin

---
+++ plugins/spark.replace.js
@@ -0,0 +1,13 @@
+/*!
+ * Replace plugin for Spark JavaScript library v1.0.0
+ * http://flowdev.co.uk/
+ * 
+ * Copyright 2010, Oliver Caldwell
+ */
+
+(function() {
+	SparkPlugin.replace = function() {
+		
+	};
+	SparkInit();
+})();

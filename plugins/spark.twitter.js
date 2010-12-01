886699835dc4bea52ade42e9d15bdeb19f074594
Oliver Caldwell <oliver@flowdev.co.uk>
2010-12-01 21:33:07 +0000

    Added bare bones file for the twitter plugin

---
+++ plugins/spark.twitter.js
@@ -0,0 +1,16 @@
+/*!
+ * Twitter plugin for Spark JavaScript library v1.0.0
+ * http://flowdev.co.uk/
+ * 
+ * Copyright 2010, Oliver Caldwell
+ */
+
+(function() {
+	// Setup the plugin
+	SparkPlugin.replace = function() {
+		
+	};
+	
+	// Reinitialise Spark
+	SparkInit();
+})();

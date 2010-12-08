e4c6f6ee984dea9d70d8f39464295bc7c34f75db
Oliver Caldwell <oliver@flowdev.co.uk>
2010-12-08 15:57:53 +0000

    Added header block comment to transition plugin

--- plugins/spark.transition.js
+++ plugins/spark.transition.js
@@ -1,3 +1,10 @@
+/*!
+ * Transition plugin for Spark JavaScript library v1.0.0
+ * http://flowdev.co.uk/
+ * 
+ * Copyright 2010, Oliver Caldwell
+ */
+
 (function() {
 	// Setup the plugin
 	SparkPlugin.transition = function(method, timeframe, callback) {

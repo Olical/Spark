b25477f03f82cf3ca6d0a7c577c508b18e94c681
Oliver Caldwell <oliver@flowdev.co.uk>
2010-12-08 16:00:40 +0000

    Finished v1.0.0 of the transition plugin

--- plugins/spark.transition.js
+++ plugins/spark.transition.js
@@ -8,7 +8,82 @@
 (function() {
 	// Setup the plugin
 	SparkPlugin.transition = function(method, timeframe, callback) {
+		// Check if we have a callback, if not set it to and empty function
+		if(callback === undefined)
+		callback = new Function();
 		
+		// Loop through all elements
+		for(var e in this.elements)
+		{
+			// Set overflow to hidden
+			$(this.selector).css({overflow: 'hidden'});
+			
+			// Work out what method we need to do
+			switch(method)
+			{
+				case 'slidedown':
+					// Display it
+					$(this.selector).css({display: 'block'});
+					
+					// Get original height
+					var original = $(this.selector).size().height;
+					
+					// Set height to 0
+					$(this.selector).size($(this.selector).size().width, 0);
+					
+					// Slide height to original
+					$(this.selector).size($(this.selector).size().width, original, timeframe, callback);
+					break;
+				
+				case 'slideup':
+					// Get original height
+					var original = $(this.selector).size().height;
+					
+					// Se the selector so it is in scope
+					var selector = this.selector;
+					
+					// Slide height to 0
+					$(this.selector).size($(this.selector).size().width, 0, timeframe, function() {
+						// Set height to original
+						$(selector).size($(selector).size().width, original);
+						
+						// Hide it
+						$(selector).css({display: 'none'});
+						
+						// Run the callback
+						callback();
+					});
+					break;
+				
+				case 'fadein':
+					// Display it
+					$(this.selector).css({display: 'block'});
+					
+					// Set opacity to 0
+					$(this.selector).opacity(0);
+					
+					// Fade opacity to 100
+					$(this.selector).opacity(100, timeframe, callback);
+					break;
+				
+				case 'fadeout':
+					// Make the in scope global
+					var selector = this.selector;
+					
+					// Fade opacity to 0
+					$(this.selector).opacity(0, timeframe, function() {
+						// Set opacity to 100
+						$(this.selector).opacity(100);
+						
+						// Hide it
+						$(selector).css({display: 'none'});
+						
+						// Run the callback
+						callback();
+					});
+					break;
+			}
+		}
 	};
 	
 	// Reinitialise Spark

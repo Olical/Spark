18d7629f76f8f920acb14e34509b7eee462e1827
Oliver Caldwell <oliver@flowdev.co.uk>
2010-12-10 21:32:18 +0000

    Added the event module

---
+++ src/event.js
@@ -0,0 +1,10 @@
+SparkFn.event = function(type, callback) {
+	for(var e in this.elements)
+	{
+		// Check if the browser supports addEventListener or attachEvent
+		if(this.elements[e].addEventListener)
+			this.elements[e].addEventListener(type, function(event) {callback(Spark.fixEvents(event))}, false);
+		else if(this.elements[e].attachEvent)
+			this.elements[e].attachEvent('on' + type, function(event) {callback(Spark.fixEvents(event))});
+	}
+};

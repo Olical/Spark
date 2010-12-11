8c85894f0cc57ed1a1a39794e4de94f2f4c68908
Oliver Caldwell <oliver@flowdev.co.uk>
2010-12-11 10:57:47 +0000

    Cleaned up the ajax modules indenting

--- src/ajax.js
+++ src/ajax.js
@@ -1,18 +1,19 @@
 SparkFn.ajax = function(method, file, data, callback) {
 	// Set up the request, allow for cross browser.
 	var xmlhttp;
+	
 	if(window.XMLHttpRequest)
 		xmlhttp = new XMLHttpRequest(); // For IE7+, Firefox, Chrome, Opera, Safari
 	else
 		xmlhttp = new ActiveXObject('Microsoft.XMLHTTP'); // For IE6, IE5
-
+	
 	// Convert to upper case.
 	method = method.toUpperCase();
-
+	
 	// If the method is get then append the data to the file string
 	if(method == 'GET' && data !== undefined && data !== false)
 		file += '?' + data;
-
+	
 	// Run the call back if it was a success and the callback is set
 	if(callback != undefined)
 		xmlhttp.onreadystatechange = function() {
@@ -20,10 +21,10 @@ SparkFn.ajax = function(method, file, data, callback) {
 				callback(xmlhttp.responseText);
 			}
 		};
-
+	
 	// Open the reader, if callback set then make it async
 	xmlhttp.open(method, file, (callback != undefined) ? true : false);
-
+	
 	// If the method is post then send the headers and the data
 	if(method == 'POST')
 	{
@@ -35,7 +36,7 @@ SparkFn.ajax = function(method, file, data, callback) {
 	}
 	else
 		xmlhttp.send(null); // Otherwise just send the data
-
+	
 	if(callback == undefined)
 		return xmlhttp.responseText;
 };

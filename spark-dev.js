4ff8015796d8efdb862e2a596ec39d0626738d3e
Oliver Caldwell <oliver@flowdev.co.uk>
2010-12-11 11:52:52 +0000

    Rebuilt with cookie module changes

--- spark-dev.js
+++ spark-dev.js
@@ -152,41 +152,46 @@ SparkFn.content = function(content, append) {
 	}
 };
 SparkFn.cookie = function(name, content, duration) {
+	// Check if content is set
 	if(!content)
 	{
-		// Return cookie's content
+		// If not, return its content
+		
+		// Set up any variables needed
 		var nameEQ = name + '=';
+		
+		// Split the cookie string
 		var ca = document.cookie.split(';');
+		
+		// Loop through all of the cookies looking for ours
 		for(var i = 0; i < ca.length; i++)
 		{
 			var c = ca[i];
+			
+			// Cut of the whitespace
 			while(c.charAt(0) == ' ')
 			{
 				c = c.substring(1, c.length);
 			}
+			
+			// If the cookie has the right name, return its contents
 			if(c.indexOf(nameEQ) == 0)
 				return c.substring(nameEQ.length, c.length);
 		}
+		
+		// If we didnt find it, return false
 		return false;
 	}
 	else
 	{
-		if(!duration)
-		{
-			// Create cookie with preset duration of one year
-			var date = new Date();
-			date.setTime(date.getTime() + 31536000000);
-			var expires = '; expires=' + date.toGMTString();
-			document.cookie = name + '=' + content + expires + '; path=/';
-		}
-		else
-		{
-			// Create cookie with specified duration
-			var date = new Date();
-			date.setTime(date.getTime() + duration);
-			var expires = '; expires=' + date.toGMTString();
-			document.cookie = name + '=' + content + expires + '; path=/';
-		}
+		// Get the current time
+		var date = new Date();
+		
+		// Push the time on by either a year or the user defined duration
+		date.setTime(date.getTime() + ((duration) ? duration : 31536000000));
+		
+		// Set the cookie
+		document.cookie = name + '=' + content + '; expires=' + date.toGMTString() + '; path=/';
 	}
 };
 SparkFn.css = function(css) {

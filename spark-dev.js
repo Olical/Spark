370682e15096e30929353bd7103b86179eb92df2
Oliver Caldwell <oliver@flowdev.co.uk>
2010-12-11 12:08:33 +0000

    Rebuilt with many core module changes

--- spark-dev.js
+++ spark-dev.js
@@ -27,24 +27,21 @@ window.SparkIn = function()
 		var result = new Object();
 		
 		// Check that they have passed arguments to the class
-		if(selector != undefined)
+		if(selector)
 		{
 			// If context then get result with context, if not just get the element
 			// This also checks if querySelectorAll is avaliable, is so it uses it instead of Sizzle
-			if(document.querySelectorAll == undefined)
+			if(!document.querySelectorAll)
 			{
-				if(context != undefined)
-					result = Sizzle(selector, context);
-				else
-					result = Sizzle(selector);
+				// Run sizzle with or without a context
+				(context) ? result = Sizzle(selector, context) : Sizzle(selector);
 			}
 			else
 			{
-				if(context != undefined)
-					var resultb = context.querySelectorAll(selector);
-				else
-					var resultb = document.querySelectorAll(selector);
+				// Run the query selector with or without a context
+				var resultb = ((context) ? context : document).querySelectorAll(selector);
 				
+				// Change the format of the returned elements so it resembles sizzle
 				for(var i = 0; i < resultb.length; ++i)
 					result[i] = resultb[i];
 			}
@@ -54,19 +51,21 @@ window.SparkIn = function()
 		var built = {
 			fps: 60,
 			selector: selector,
-			elements: result
+			elements: result,
+			library: 'Spark'
 		};
 		
+		// Add in the functions
 		for(var p in SparkFn)
 			built[p] = SparkFn[p];
-	
+		
 		// Return the functions
 		return built;
 	};
-
+	
 	// Function for making the pageX/Y values work in IE
 	Spark.fixEvents = function(theEvent) {
-		if(theEvent.pageX == null)
+		if(!theEvent.pageX)
 		{
 			var d = (document.documentElement && document.documentElement.scrollLeft != null) ? document.documentElement : document.body;
 			docX = theEvent.clientX + d.scrollLeft;
@@ -74,15 +73,15 @@ window.SparkIn = function()
 			theEvent.pageX = docX;
 			theEvent.pageY = docY;
 		}
-	
+		
 		if(!theEvent.target)
-			theEvent.target == theEvent.srcElement;
-	
+			theEvent.target = theEvent.srcElement;
+		
 		// Return the calculated positions in an object
 		return theEvent;
 	};
-
-	// Take out the need for brackets on functions that do not need an element
+	
+	// Take out the need for brackets
 	for(var i in Spark())
 		Spark[i] = $[i] = Spark()[i];
 };

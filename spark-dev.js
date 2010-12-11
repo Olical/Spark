b145dc159b77ef2bfbd46f60f4a2a7134e1f855b
Oliver Caldwell <oliver@flowdev.co.uk>
2010-12-11 11:24:32 +0000

    Built with the optimised ajax module

--- spark-dev.js
+++ spark-dev.js
@@ -88,43 +88,41 @@ window.SparkIn = function()
 };
 SparkFn.ajax = function(method, file, data, callback) {
 	// Set up the request, allow for cross browser.
-	var xmlhttp;
-	if(window.XMLHttpRequest)
-		xmlhttp = new XMLHttpRequest(); // For IE7+, Firefox, Chrome, Opera, Safari
-	else
-		xmlhttp = new ActiveXObject('Microsoft.XMLHTTP'); // For IE6, IE5
-
+	var xmlhttp = (XMLHttpRequest) ?
+		new XMLHttpRequest :
+		new ActiveXObject('Microsoft.XMLHTTP');
+	
 	// Convert to upper case.
 	method = method.toUpperCase();
-
+	
 	// If the method is get then append the data to the file string
-	if(method == 'GET' && data !== undefined && data !== false)
+	if(method == 'GET' && data)
 		file += '?' + data;
-
+	
 	// Run the call back if it was a success and the callback is set
-	if(callback != undefined)
+	if(callback)
 		xmlhttp.onreadystatechange = function() {
 			if(xmlhttp.readyState == 4) {
 				callback(xmlhttp.responseText);
 			}
 		};
-
+	
 	// Open the reader, if callback set then make it async
-	xmlhttp.open(method, file, (callback != undefined) ? true : false);
-
+	xmlhttp.open(method, file, (callback) ? true : false);
+	
 	// If the method is post then send the headers and the data
 	if(method == 'POST')
 	{
 		xmlhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-		if(data !== undefined && data !== false)
-			xmlhttp.send(data);
-		else
-			xmlhttp.send(null);
+		
+		// Only send the data if it is set
+		xmlhttp.send((data) ? data : null);
 	}
+	// Otherwise just send the data
 	else
-		xmlhttp.send(null); // Otherwise just send the data
-
-	if(callback == undefined)
+		xmlhttp.send(null);
+	
+	if(!callback)
 		return xmlhttp.responseText;
 };
 SparkFn.attribute = function(attribute) {

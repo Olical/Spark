865f68e5adc075ebc5dc67b7fc8158f45a9777df
Oliver Caldwell <oliver@flowdev.co.uk>
2011-01-14 20:58:59 +0000

    Replaced more of the word element with this.elements[e]

--- src/animate.js
+++ src/animate.js
@@ -35,7 +35,7 @@ SparkFn.animate = function(properties, timeframe, callback) {
 						return function() {
 							extelement.style[extp] = extoriginal + (extpixels * exti) + extunit;
 						}
-					})(i, element, p, original, pixels, unit), i * (1000 / 60), this.elements[e], p, original, pixels, unit);
+					})(i, this.elements[e], p, original, pixels, unit), i * (1000 / 60), this.elements[e], p, original, pixels, unit);
 				}
 				
 				// Correct floating point problem
@@ -43,7 +43,7 @@ SparkFn.animate = function(properties, timeframe, callback) {
 					return function() {
 						extelement.style[extp] = extproperties[extp] + extunit;
 					}
-				})(i, element, p, properties, unit), timeframe, this.elements[e], p, properties, unit);
+				})(i, this.elements[e], p, properties, unit), timeframe, this.elements[e], p, properties, unit);
 		}
 	}
 	

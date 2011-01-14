6fe5fbb9b27d99a41870b15355dca8ec499a3b06
Oliver Caldwell <oliver@flowdev.co.uk>
2011-01-14 20:47:33 +0000

    Added the main loops to the animate function

--- src/animate.js
+++ src/animate.js
@@ -1,3 +1,12 @@
 SparkFn.animate = function(properties, timeframe, callback) {
-	
+	// Loop through all the elements
+	for(var e in this.elements) {
+		// Set a default timeframe
+		if(timeframe === undefined) timeframe = 800;
+		
+		// Loop through all of the properties
+		for(var p in properties) {
+			
+		}
+	}
 };

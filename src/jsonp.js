f2b5c99aacd6da24311d6f10042ddf9d6dea407b
Oliver Caldwell <oliver@flowdev.co.uk>
2011-01-15 17:18:25 +0000

    Added what I presume to be a working copy of the jsonp function

--- src/jsonp.js
+++ src/jsonp.js
@@ -1,3 +1,21 @@
-SparkFn.jsonp = function() {
+SparkFn.jsonp = function(file, callback, parameters) {
+	// Grab the head element
+	var head = document.getElementsByTagName('head')[0];
 	
+	// Create a script element
+	var script = document.createElement('script');
+	
+	// Set the type
+	script.type = 'text/javascript';
+	
+	// Set the source file
+	script.src = file + '?callback=' + callback;
+	
+	// Loop through the parameters adding them to the src
+	for(p in parameters) {
+		script.src += '&' + parameters[p];
+	}
+	
+	// Add the script element to the head
+	head.appendChild(script);
 };

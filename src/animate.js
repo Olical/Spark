f87e735fdad55c175d40197e95413c33d802fdc3
Oliver Caldwell <oliver@flowdev.co.uk>
2011-01-15 12:53:18 +0000

    Changed the indenting

--- src/animate.js
+++ src/animate.js
@@ -17,44 +17,44 @@ SparkFn.animate = function(properties, timeframe, callback) {
 			this.elements[e].style[p] = (window.getComputedStyle) ?
 				window.getComputedStyle(this.elements[e], null)[p] :
 				this.elements[e].currentStyle[p];
-				
-				// Fix for IE stuff
-				if(this.elements[e].style[p] == 'auto') this.elements[e].style[p] = 0;
-				
-				// Get the original
-				var original = (p == 'opacity' || p == 'MozOpacity' || p == 'khtmlOpacity') ? parseFloat(this.elements[e].style[p]) : parseInt(this.elements[e].style[p]);
-				
-				// Work out the difference
-				var difference = (p == 'opacity' || p == 'MozOpacity' || p == 'khtmlOpacity') ? parseFloat(properties[p]) - original  : parseInt(properties[p]) - original;
-				
-				// Work out how many frames
-				var frames = timeframe / (1000 / 60);
-				
-				// Work out how many pixels per frame
-				var pixels = difference / frames;
-				
-				// Work out the unit of measurement
-				var unit = (isNaN(properties[p])) ? properties[p].replace(/[0-9]/g, '') : 'px';
-				
-				// Another opacity fix
-				if(properties.opacity)
-					unit = false;
-				
-				// Loop through each frame
-				for(var i = 0; i <= frames; i++) {
-					setTimeout((function(exti, extelement, extp, extoriginal, extpixels, extunit) {
-						return function() {
-							extelement.style[extp] = extoriginal + (extpixels * exti) + extunit;
-						}
-					})(i, this.elements[e], p, original, pixels, unit), i * (1000 / 60), this.elements[e], p, original, pixels, unit);
-				}
-				
-				// Correct floating point problem
-				setTimeout((function(exti, extelement, extp, extproperties, extunit) {
+			
+			// Fix for IE stuff
+			if(this.elements[e].style[p] == 'auto') this.elements[e].style[p] = 0;
+			
+			// Get the original
+			var original = (p == 'opacity' || p == 'MozOpacity' || p == 'khtmlOpacity') ? parseFloat(this.elements[e].style[p]) : parseInt(this.elements[e].style[p]);
+			
+			// Work out the difference
+			var difference = (p == 'opacity' || p == 'MozOpacity' || p == 'khtmlOpacity') ? parseFloat(properties[p]) - original  : parseInt(properties[p]) - original;
+			
+			// Work out how many frames
+			var frames = timeframe / (1000 / 60);
+			
+			// Work out how many pixels per frame
+			var pixels = difference / frames;
+			
+			// Work out the unit of measurement
+			var unit = (isNaN(properties[p])) ? properties[p].replace(/[0-9]/g, '') : 'px';
+			
+			// Another opacity fix
+			if(properties.opacity)
+				unit = false;
+			
+			// Loop through each frame
+			for(var i = 0; i <= frames; i++) {
+				setTimeout((function(exti, extelement, extp, extoriginal, extpixels, extunit) {
 					return function() {
-						extelement.style[extp] = extproperties[extp] + extunit;
+						extelement.style[extp] = extoriginal + (extpixels * exti) + extunit;
 					}
-				})(i, this.elements[e], p, properties, unit), timeframe, this.elements[e], p, properties, unit);
+				})(i, this.elements[e], p, original, pixels, unit), i * (1000 / 60), this.elements[e], p, original, pixels, unit);
+			}
+			
+			// Correct floating point problem
+			setTimeout((function(exti, extelement, extp, extproperties, extunit) {
+				return function() {
+					extelement.style[extp] = extproperties[extp] + extunit;
+				}
+			})(i, this.elements[e], p, properties, unit), timeframe, this.elements[e], p, properties, unit);
 		}
 	}
 	

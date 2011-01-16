2e1e31f3780bbff384496afda4c43288e8891cfb
Oliver Caldwell <oliver@flowdev.co.uk>
2011-01-16 13:36:22 +0000

    Changed jsonp.js so it works off the load function and also does not require parameters anymore (now optional)

--- src/jsonp.js
+++ src/jsonp.js
@@ -1,22 +1,16 @@
 SparkFn.jsonp = function(file, callback, parameters) {
-	// Grab the head element
-	var head = document.getElementsByTagName('head')[0];
+	// Add the callback
+	file += '?callback=' + callback;
 	
-	// Create a script element
-	var script = document.createElement('script');
-	
-	// Set the type
-	script.type = 'text/javascript';
-	
-	// Set the source file
-	script.src = file + '?callback=' + callback;
-	
-	// Loop through the parameters
-	for(p in parameters) {
-		// And an ampersand and the specified parameter
-		script.src += '&' + parameters[p];
+	// If we have parameters
+	if(parameters !== undefined) {
+		// Loop through the parameters
+		for(p in parameters) {
+			// And an ampersand and the specified parameter
+			file += '&' + parameters[p];
+		}
 	}
 	
-	// Add the script element to the head
-	head.appendChild(script);
+	// Load the file
+	this.load(file);
 };

cdcfa853a0a7a38384b98361ad0a2911d110acb4
Oliver Caldwell <oliver@flowdev.co.uk>
2011-01-17 09:09:07 +0000

    Made the jsonp function just add the parameters as a string, rather than looping through

--- src/jsonp.js
+++ src/jsonp.js
@@ -2,14 +2,9 @@ SparkFn.jsonp = function(file, callback, parameters) {
 	// Add the callback
 	file += '?callback=' + callback;
 	
-	// If we have parameters
-	if(parameters !== undefined) {
-		// Loop through the parameters
-		for(p in parameters) {
-			// And an ampersand and the specified parameter
-			file += '&' + parameters[p];
-		}
-	}
+	// If we have parameters add them
+	if(parameters !== undefined)
+		file += '&' + parameters;
 	
 	// Load the file
 	this.load(file);

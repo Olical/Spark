5185e76c6e280432dd06124d57254f5f9d4dfd56
Oliver Caldwell <oliver@flowdev.co.uk>
2011-01-17 09:15:04 +0000

    Remade with the changes to the jsonp function

--- spark-dev.js
+++ spark-dev.js
@@ -443,14 +443,9 @@ SparkFn.browser = function() {
 	// Add the callback
 	file += '?callback=' + callback;
 	
-	// If we have parameters
-	if(parameters !== undefined) {
-		// Loop through the parameters
-		for(p in parameters) {
-			// And an ampersand and the specified parameter
-			file += '&' + parameters[p];
-		}
-	}
+	// If we have parameters add them
+	if(parameters !== undefined)
+		file += '&' + parameters;
 	
 	// Load the file
 	this.load(file);

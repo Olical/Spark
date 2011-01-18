ef8ae6ed882b714d7cfde5eb7a0526d447f3d302
Oliver Caldwell <oliver@flowdev.co.uk>
2011-01-18 13:11:19 +0000

    Got the stop function working

--- src/stop.js
+++ src/stop.js
@@ -1,3 +1,16 @@
 SparkFn.stop = function() {
-	
+	// Loop through each of the elements
+	for(var e in this.elements) {
+		// Make sure it is set
+		if(this.data(this.elements[e], 'Spark.animations') === undefined)
+			this.data(this.elements[e], 'Spark.animations', '');
+		
+		// Get the animations
+		var animations = this.data(this.elements[e], 'Spark.animations').split(',');
+		
+		// Loop through them all, canceling them all
+		for(var a in animations) {
+			clearTimeout(animations[a]);
+		}
+	}
 };

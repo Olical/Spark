85188d6d0fb298460d422d2c101d7ac19894b1fb
Oliver Caldwell <oliver@flowdev.co.uk>
2011-01-19 22:04:35 +0000

    Remade with the animate function fix

--- spark-dev.js
+++ spark-dev.js
@@ -451,7 +451,7 @@ SparkFn.browser = function() {
 			// Correct floating point problem
 			this.data(this.elements[e], 'Spark.animations', this.data(this.elements[e], 'Spark.animations') + ',' + setTimeout((function(extelement, extp, extproperties, extunit, extprefix) {
 				return function() {
-					extelement.style[extp] = extprefix + extproperties[extp] + extunit;
+					extelement.style[extp] = extprefix + parseInt(extproperties[extp]) + extunit;
 				}
 			})(this.elements[e], p, properties, unit, prefix), timeframe, this.elements[e], p, properties, unit, prefix));
 			

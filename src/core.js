1bacb22d1cd8f4d08a1109bbb8c848df1de559a8
Oliver Caldwell <oliver@flowdev.co.uk>
2011-01-22 12:54:18 +0000

    Elaborated on a a comment in the core file

--- src/core.js
+++ src/core.js
@@ -26,7 +26,7 @@ window.SparkIn = function()
 		// Create the result object
 		var result = new Object();
 		
-		// Check that they have passed arguments to the class
+		// Check that they have passed arguments to the class and also if they have passed an element or a string
 		if(selector !== undefined) {
 			if(typeof selector == 'string') {
 				// Run sizzle with or without a context

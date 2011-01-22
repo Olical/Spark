dfdcfc06707b804c45e1995f9dba73c576062e7c
Oliver Caldwell <oliver@flowdev.co.uk>
2011-01-22 14:02:28 +0000

    Upgraded the twitter plugin to use the jsonp function

--- plugins/spark.twitter.js
+++ plugins/spark.twitter.js
@@ -11,28 +11,11 @@
 	
 	// Setup the plugin
 	SparkFn.twitter = function(user) {
-		// Set up the callback function
-		window.twitterPluginDataLoaded = function(data) {
-			$(twitterPluginData[data.query]).content(data.results[0].text);
-		};
+		Spark.jsonp('http://search.twitter.com/search.json', 'sparkTwitterPluginLoaded', 'q=from%3A' + user);
 		
-		// Grab the head element
-		var head = document.getElementsByTagName('head')[0];
-		
-		// Create a script element
-		var script = document.createElement('script');
-		
-		// Set the type
-		script.type = 'text/javascript';
-		
-		// Set the source file
-		script.src = 'http://search.twitter.com/search.json?callback=twitterPluginDataLoaded&q=from%3A' + user;
-		
-		// Just so we can access it, because we can pass parameters
-		twitterPluginData['from%3A' + user] = this.selector;
-		
-		// Add the script element to the head
-		head.appendChild(script);
+		window.sparkTwitterPluginLoaded = function(data) {
+		   Spark(this.elements).content(data.results[0].text);
+		}
 	};
 	
 	// Reinitialise Spark

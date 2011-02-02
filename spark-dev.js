1802bf5b3bae9cb323fe9439f6ee7bd70cabf59c
Oliver Caldwell <oliver@flowdev.co.uk>
2011-02-02 22:06:16 +0000

    Remade with the ready module

--- spark-dev.js
+++ spark-dev.js
@@ -150,7 +150,33 @@ window.SparkIn = function() {
 	
 	// Return the calculated positions
 	return e;
-};(function(){
+};SparkFn.ready = function(callback) {
+	// Set up variables
+	window.alreadyRunFlag = 0;
+	
+	// Check if we can add an event listener
+	if(document.addEventListener) {
+		document.addEventListener("DOMContentLoaded", function() { alreadyRunFlag = 1; callback() }, false);
+	}
+	else if(document.all && !window.opera) {
+		document.write('<script type="text/javascript" id="contentloadtag" defer="defer" src="javascript:void(0)"></script>');
+		
+		var contentloadtag = document.getElementById('contentloadtag');
+		
+		contentloadtag.onreadystatechange = function() {
+			if(this.readyState == 'complete')
+			{
+				alreadyrunflag = 1;
+				callback();
+			}
+		};
+	}
+	
+	window.onload = function() {
+		setTimeout("if(!alreadyRunFlag) callback()", 0);
+	};
+};
+(function(){
 
 var chunker = /((?:\((?:\([^()]+\)|[^()]+)+\)|\[(?:\[[^\[\]]*\]|['"][^'"]*['"]|[^\[\]'"]+)+\]|\\.|[^ >+~,(\[\\]+)+|[>+~])(\s*,\s*)?((?:.|\r|\n)*)/g,
 	done = 0,

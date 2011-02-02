3db246af65ff804d7619a26eff4cb52fdda799ab
Oliver Caldwell <oliver@flowdev.co.uk>
2011-02-02 21:48:07 +0000

    Added the content for the html function

---
+++ src/html.js
@@ -0,0 +1,24 @@
+SparkFn.html = function(content, append) {
+	// Set up any variables
+	var element = null;
+	
+	// Loop through all of the elements
+	for(var e in this.elements) {
+		// Grab the current element
+		element = this.elements[e];
+		
+		// Return content of the selected element if there is no content
+		if(content === undefined) {
+			return element.innerHTML;
+		}
+		else {
+			// Append or replace content depending on the append flag
+			(!append) ?
+				element.innerHTML = content :
+				element.innerHTML += content;
+		}
+	}
+	
+	// Return the Spark object
+	return this;
+};

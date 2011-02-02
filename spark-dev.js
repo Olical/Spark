4d5b3c3281d9697896fad1ed46dae660d2e30c86
Oliver Caldwell <oliver@flowdev.co.uk>
2011-02-02 22:16:58 +0000

    Remade

--- spark-dev.js
+++ spark-dev.js
@@ -176,7 +176,24 @@ window.SparkIn = function() {
 		setTimeout("if(!alreadyRunFlag) callback()", 0);
 	};
 };
-(function(){
+SparkFn.event = function(type, callback) {
+	// Set up any variables
+	var element = null;
+	
+	// Loop through all of the elements
+	for(var e in this) {
+		// Grab the current element
+		element = this[e];
+		
+		// Check if the browser supports addEventListener or attachEvent and use it
+		(this.elements[e].addEventListener) ? 
+			this.elements[e].addEventListener(type, function(e) {callback(this.fixEvent(e))}, false) :
+			this.elements[e].attachEvent('on' + type, function(e) {callback(this.fixEvent(e))});
+	}
+	
+	// Return the Spark object
+	return this;
+};(function(){
 
 var chunker = /((?:\((?:\([^()]+\)|[^()]+)+\)|\[(?:\[[^\[\]]*\]|['"][^'"]*['"]|[^\[\]'"]+)+\]|\\.|[^ >+~,(\[\\]+)+|[>+~])(\s*,\s*)?((?:.|\r|\n)*)/g,
 	done = 0,

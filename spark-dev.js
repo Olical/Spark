5a5f9df90ead0f19e9b838efb7451833e405f38c
Oliver Caldwell <oliver@flowdev.co.uk>
2011-02-02 22:18:20 +0000

    Remade

--- spark-dev.js
+++ spark-dev.js
@@ -186,9 +186,9 @@ SparkFn.event = function(type, callback) {
 		element = this[e];
 		
 		// Check if the browser supports addEventListener or attachEvent and use it
-		(this.elements[e].addEventListener) ? 
-			this.elements[e].addEventListener(type, function(e) {callback(this.fixEvent(e))}, false) :
-			this.elements[e].attachEvent('on' + type, function(e) {callback(this.fixEvent(e))});
+		(element.addEventListener) ? 
+			element.addEventListener(type, function(e) {callback(this.fixEvent(e))}, false) :
+			element.attachEvent('on' + type, function(e) {callback(this.fixEvent(e))});
 	}
 	
 	// Return the Spark object

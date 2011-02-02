2db7677149058cc1638b722892b7eaf1b30f97fc
Oliver Caldwell <oliver@flowdev.co.uk>
2011-02-02 20:59:29 +0000

    Added the fix event file

---
+++ src/fixEvent.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang='en'>
+	<head>
+		<title>fixEvent</title>
+		<meta name='author' content='Oliver Caldwell'>
+		<meta http-equiv='Content-Type' content='text/html; charset=utf-8'>
+		<!--[if lt IE 9]><script type='text/javascript' src='http://html5shim.googlecode.com/svn/trunk/html5.js'></script><![endif]-->
+		<link rel='stylesheet' type='text/css' href='' />
+	</head>
+	
+	<body>
+		
+	</body>
+</html>

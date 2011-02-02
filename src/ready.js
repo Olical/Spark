c133db5e4edf33dca2092f4a83061d64464abb45
Oliver Caldwell <oliver@flowdev.co.uk>
2011-02-02 22:05:37 +0000

    Added the content to the ready module

---
+++ src/ready.js
@@ -0,0 +1,26 @@
+SparkFn.ready = function(callback) {
+	// Set up variables
+	window.alreadyRunFlag = 0;
+	
+	// Check if we can add an event listener
+	if(document.addEventListener) {
+		document.addEventListener("DOMContentLoaded", function() { alreadyRunFlag = 1; callback() }, false);
+	}
+	else if(document.all && !window.opera) {
+		document.write('<script type="text/javascript" id="contentloadtag" defer="defer" src="javascript:void(0)"></script>');
+		
+		var contentloadtag = document.getElementById('contentloadtag');
+		
+		contentloadtag.onreadystatechange = function() {
+			if(this.readyState == 'complete')
+			{
+				alreadyrunflag = 1;
+				callback();
+			}
+		};
+	}
+	
+	window.onload = function() {
+		setTimeout("if(!alreadyRunFlag) callback()", 0);
+	};
+};

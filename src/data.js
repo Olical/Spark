ad01361e43f957c6af8020103e04ce641746fcbd
Oliver Caldwell <oliver@flowdev.co.uk>
2011-02-02 17:18:26 +0000

    Put the data function back to how it was

--- src/data.js
+++ src/data.js
@@ -1,15 +1,17 @@
-SparkFn.data =  function (el, key, value) {
+SparkFn.data = (function () {
 	// Set up the variables
 	var storage = {};
 	var counter = 1;
 	
-	var uid = el.uniqueID || (el.uniqueID = counter++);
-	storage[uid] || (storage[uid] = {});
-	
-	if(value !== undefined) {
-		storage[uid][key] = value;
-	}
-	else {
-		return storage[uid][key];
-	}
-};
+	return function (el, key, value) {
+		var uid = el.uniqueID || (el.uniqueID = counter++);
+		storage[uid] || (storage[uid] = {});
+		
+		if(typeof value != "undefined") {
+			storage[uid][key] = value;
+		}
+		else {
+			return storage[uid][key];
+		}
+	};
+})();

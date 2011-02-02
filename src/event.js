c30aa81702f47c68c761faab6c943e260abae758
Oliver Caldwell <oliver@flowdev.co.uk>
2011-02-02 16:33:09 +0000

    Changed the formatting of the event function slightly

--- src/event.js
+++ src/event.js
@@ -1,6 +1,5 @@
 SparkFn.event = function(type, callback) {
-	for(var e in this.elements)
-	{
+	for(var e in this.elements) {
 		// Check if the browser supports addEventListener or attachEvent
 		if(this.elements[e].addEventListener)
 			this.elements[e].addEventListener(type, function(event) {callback(Spark.fixEvents(event))}, false);

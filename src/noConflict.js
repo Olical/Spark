218fd4e73ec0a519a2cc5ed05a8b1a9d2986d9a4
Oliver Caldwell <oliver@flowdev.co.uk>
2011-02-04 20:46:44 +0000

    Added the initial content to the no conflict function, may not work yet

--- src/noConflict.js
+++ src/noConflict.js
@@ -1,3 +1,16 @@
 SparkFn.noConflict = function(both) {
+	// Grab the original Spark object
+	var SparkOr = Spark;
 	
+	// Reload $
+	window.$ = SparkBk.$;
+	
+	// If both is true
+	if(both === true) {
+		// Reload Spark
+		window.Spark = SparkBk.Spark;
+	}
+	
+	// Return the original Spark object
+	return SparkOr;
 };

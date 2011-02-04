0df4f0487c7dc56c851c3287f731f9c7df1deff3
Oliver Caldwell <oliver@flowdev.co.uk>
2011-02-04 13:00:00 +0000

    Rebuilt with the css function

--- spark-dev.js
+++ spark-dev.js
@@ -395,7 +395,39 @@ SparkFn.event = function(type, callback) {
 		document.cookie = name + '=' + content + '; expires=' + date.toGMTString() + '; path=/';
 	}
 };
-(function(){
+SparkFn.css = function(css) {
+	// Set up any variables
+	var element = null;
+	
+	// Loop through all of the elements
+	for(var e in this.elements) {
+		// Grab the current element
+		element = this.elements[e];
+		
+		// Check if they provided a css object
+		if(css !== undefined) {
+			// Loop through all css values assigning them
+			for(var c in css) {
+				element.style[c] = css[c];
+				
+				// If opacity is being set we need to set all the other values for cross browser opacity
+				if(c == 'opacity') {
+					element.style.MozOpacity = css[c];
+					element.style.KhtmlOpacity = css[c];
+					element.style.filter = 'alpha(opacity=' + (css[c] * 100) + ')';
+					element.style.zoom = '1';
+				}
+			}
+		}
+		else {
+			// Return the elements attributes
+			return element.style;
+		}
+	}
+	
+	// Return the Spark object
+	return this;
+};(function(){
 
 var chunker = /((?:\((?:\([^()]+\)|[^()]+)+\)|\[(?:\[[^\[\]]*\]|['"][^'"]*['"]|[^\[\]'"]+)+\]|\\.|[^ >+~,(\[\\]+)+|[>+~])(\s*,\s*)?((?:.|\r|\n)*)/g,
 	done = 0,

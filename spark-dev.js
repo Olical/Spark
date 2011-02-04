77296c087dcba684d8f9874eab258417c13bcd69
Oliver Caldwell <oliver@flowdev.co.uk>
2011-02-04 13:10:02 +0000

    Remade with the data function

--- spark-dev.js
+++ spark-dev.js
@@ -427,7 +427,30 @@ SparkFn.css = function(css) {
 	
 	// Return the Spark object
 	return this;
-};(function(){
+};SparkFn.data = (function () {
+	// Set up the variables
+	var storage = {};
+	var counter = 1;
+	
+	// Return the function to manage saving data
+	return function (el, key, value) {
+		// Get the unique id
+		var uid = el.uniqueID || (el.uniqueID = counter++);
+		
+		// Set up a place to store the data
+		storage[uid] || (storage[uid] = {});
+		
+		// Check if a value has been passed
+		if(typeof value != 'undefined') {
+			// Set the value
+			storage[uid][key] = value;
+		}
+		else {
+			// Return the value
+			return storage[uid][key];
+		}
+	};
+})();(function(){
 
 var chunker = /((?:\((?:\([^()]+\)|[^()]+)+\)|\[(?:\[[^\[\]]*\]|['"][^'"]*['"]|[^\[\]'"]+)+\]|\\.|[^ >+~,(\[\\]+)+|[>+~])(\s*,\s*)?((?:.|\r|\n)*)/g,
 	done = 0,

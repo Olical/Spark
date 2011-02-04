87826de6943e5c47d2b1161458268cdc2c428e64
Oliver Caldwell <oliver@flowdev.co.uk>
2011-02-04 13:15:22 +0000

    Remade with the each function

--- spark-dev.js
+++ spark-dev.js
@@ -450,7 +450,22 @@ SparkFn.css = function(css) {
 			return storage[uid][key];
 		}
 	};
-})();(function(){
+})();SparkFn.each = function(callback) {
+	// Set up any variables
+	var element = null;
+	
+	// Loop through all of the elements
+	for(var e in this.elements) {
+		// Grab the current element
+		element = this.elements[e];
+		
+		// Run the specified function and pass the element to it
+		callback(element);
+	}
+	
+	// Return the Spark object
+	return this;
+};(function(){
 
 var chunker = /((?:\((?:\([^()]+\)|[^()]+)+\)|\[(?:\[[^\[\]]*\]|['"][^'"]*['"]|[^\[\]'"]+)+\]|\\.|[^ >+~,(\[\\]+)+|[>+~])(\s*,\s*)?((?:.|\r|\n)*)/g,
 	done = 0,

f49bc335766a619200df24dbaccf86f32ed44149
Oliver Caldwell <oliver@flowdev.co.uk>
2011-02-04 20:55:47 +0000

    Added the jsonp function

---
+++ src/jsonp.js
@@ -0,0 +1,12 @@
+SparkFn.jsonp = function(file, callback, parameters) {
+	// Add the callback
+	file += '?callback=' + callback;
+	
+	// If we have parameters add them
+	if(parameters !== undefined) {
+		file += '&' + parameters;
+	}
+	
+	// Load the file
+	this.load(file);
+};

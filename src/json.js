3cbaffbb429452600f606c3df29342d3f2bc0ad9
Oliver Caldwell <oliver@flowdev.co.uk>
2011-02-04 20:26:53 +0000

    Made the json function implement json2.js

--- src/json.js
+++ src/json.js
@@ -1,9 +1,9 @@
 SparkFn.json = function(method, data) {
 	// Check what we need to do
 	if(method == 'encode') {
-		
+		return JSON.stringify(data);
 	}
 	else if(method == 'decode') {
-		
+		return JSON.parse(data);
 	}
 };

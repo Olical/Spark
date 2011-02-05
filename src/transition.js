e8d7c92d0ec1002fbe0b8443c9c4f1e04d69be14
Oliver Caldwell <oliver@flowdev.co.uk>
2011-02-05 16:03:01 +0000

    Changed the transition function again, took out more fixes that where not working and made opacity animating functios animate to .01 rather than 0

--- src/transition.js
+++ src/transition.js
@@ -60,7 +60,7 @@ SparkFn.transition = function(method, timeframe, callback) {
 			
 			case 'fadeout':
 				// Fade opacity to 0
-				Spark(element).animate({opacity: 0}, timeframe, function() {
+				Spark(element).animate({opacity: .01}, timeframe, function() {
 					// Set opacity to 100
 					Spark(element).css({opacity: 1, display: 'none'});
 					
@@ -88,10 +88,10 @@ SparkFn.transition = function(method, timeframe, callback) {
 				var original = Spark(element).attribute().offsetHeight;
 				
 				// Set overflow to hidden
-				Spark(element).css({display: 'block', overflow: 'hidden', height: original, opacity: 1});
+				Spark(element).css({overflow: 'hidden', height: original});
 				
 				// Slide height to 0
-				Spark(element).animate({height: 0, opacity: 0}, timeframe, function() {
+				Spark(element).animate({height: 0, opacity: .01}, timeframe, function() {
 					// Set height to original
 					Spark(element).css({height: original, display: 'none'});
 					

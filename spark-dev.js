9fa0c16ec05e94f861883c6ba4a1040fa4c0c522
Oliver Caldwell <oliver@flowdev.co.uk>
2011-02-06 10:30:58 +0000

    Took the toString converting out of the css function.

--- spark-dev.js
+++ spark-dev.js
@@ -413,13 +413,13 @@ SparkFn.css = function(css) {
 		if(css !== undefined) {
 			// Loop through all css values assigning them
 			for(var c in css) {
-				element.style[c] = css[c].toString();
+				element.style[c] = css[c];
 				
 				// If opacity is being set we need to set all the other values for cross browser opacity
 				if(c == 'opacity') {
-					element.style.MozOpacity = css[c].toString();
-					element.style.KhtmlOpacity = css[c].toString();
-					element.style.filter = 'alpha(opacity=' + (css[c] * 100).toString() + ')';
+					element.style.MozOpacity = css[c];
+					element.style.KhtmlOpacity = css[c];
+					element.style.filter = 'alpha(opacity=' + (css[c] * 100) + ')';
 					element.style.zoom = '1';
 				}
 			}

11913e3f134442a064297351cad15c8ba98a9b96
Oliver Caldwell <oliver@flowdev.co.uk>
2011-02-06 10:20:22 +0000

    Remade

--- spark-dev.js
+++ spark-dev.js
@@ -145,20 +145,6 @@ window.SparkIn = function() {
 		e.target = e.srcElement;
 	}
 	
-	// Fix the offsetX/Y in Firefox
-	var offsetX = offsetY = 0;
-	var obj = e.target;
-	
-	if(obj.offsetParent && this.client().browser == 'Firefox') {
-		do {
-			offsetX += obj.offsetLeft;
-			offsetY += obj.offsetTop;
-		} while(obj = obj.offsetParent);
-	
-		e.offsetX = offsetX;
-		e.offsetY = offsetY;
-	}
-	
 	// Return the calculated positions
 	return e;
 };SparkFn.ready = function(callback) {

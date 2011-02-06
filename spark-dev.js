a6451dc3c7fd46f67d0a136fff1c2112401ae132
Oliver Caldwell <oliver@flowdev.co.uk>
2011-02-06 10:27:16 +0000

    Made the css function convert its values to string

--- spark-dev.js
+++ spark-dev.js
@@ -413,13 +413,13 @@ SparkFn.css = function(css) {
 		if(css !== undefined) {
 			// Loop through all css values assigning them
 			for(var c in css) {
-				element.style[c] = css[c];
+				element.style[c] = css[c].toString();
 				
 				// If opacity is being set we need to set all the other values for cross browser opacity
 				if(c == 'opacity') {
-					element.style.MozOpacity = css[c];
-					element.style.KhtmlOpacity = css[c];
-					element.style.filter = 'alpha(opacity=' + (css[c] * 100) + ')';
+					element.style.MozOpacity = css[c].toString();
+					element.style.KhtmlOpacity = css[c].toString();
+					element.style.filter = 'alpha(opacity=' + (css[c] * 100).toString() + ')';
 					element.style.zoom = '1';
 				}
 			}

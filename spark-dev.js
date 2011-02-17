ea11668c1179f92492c439da441aa950bc0600c0
Oliver Caldwell <oliver@flowdev.co.uk>
2011-02-17 12:39:18 +0000

    Remade with the new add function

--- spark-dev.js
+++ spark-dev.js
@@ -894,6 +894,33 @@ SparkFn.css = function(css) {
 	
 	// Return the Spark object
 	return this;
+};SparkFn.add = function(tag, attributes, styles) {
+	// Create the new element
+	var construct = document.createElement(tag);
+	
+	// Make sure attributes is set
+	if(attributes) {
+		// Add attributes
+		Spark(construct).attribute(attributes);
+	}
+	
+	// Make sure styles is set
+	if(styles) {
+		// Add styles
+		Spark(construct).css(styles);
+	}
+	
+	// Loop through all elements
+	for(var e in this.elements) {
+		// Make sure that it is an element
+		if(this.elements.hasOwnProperty(e)) {
+			// Add the element
+			this.elements[e].appendChild(construct);
+		}
+	}
+	
+	// Return the Spark object to allow chaining
+	return this;
 };(function(){
 
 var chunker = /((?:\((?:\([^()]+\)|[^()]+)+\)|\[(?:\[[^\[\]]*\]|['"][^'"]*['"]|[^\[\]'"]+)+\]|\\.|[^ >+~,(\[\\]+)+|[>+~])(\s*,\s*)?((?:.|\r|\n)*)/g,

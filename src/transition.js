91bc9b49f0f5972414cbff4cfdcdc75ef5461b64
Oliver Caldwell <oliver@flowdev.co.uk>
2011-02-18 19:12:13 +0000

    Cleaned up the indenting in the transition function

--- src/transition.js
+++ src/transition.js
@@ -29,85 +29,85 @@ SparkFn.transition = function(method, timeframe, easing callback) {
 		if(this.elements.hasOwnProperty(e)) {
 			// Grab the current element
 			element = this.elements[e];
-		
+			
 			// Work out what method we need to do
 			switch(method) {
 				case 'slidedown':
 					// Set overflow to hidden
 					Spark(element).css({overflow: 'hidden', display: 'block'});
-				
+					
 					// Get original height
 					original = Spark(element).attribute().offsetHeight;
-				
+					
 					// Set height to 0
 					Spark(element).css({height: 0});
-				
+					
 					// Slide height to original
 					Spark(element).animate({height: original}, timeframe, easing, callback);
 					break;
-			
+				
 				case 'slideup':				
 					// Get original height
 					original = Spark(element).attribute().offsetHeight;
-				
+					
 					// Set overflow to hidden
 					Spark(element).css({overflow: 'hidden', height: original});
-				
+					
 					// Slide height to 0
 					Spark(element).animate({height: 0}, timeframe, easing, function() {
 						// Set height to original
 						Spark(element).css({height: original + 'px', display: 'none'});
-					
+						
 						// Run the callback
 						callback();
 					});
 					break;
-			
+				
 				case 'fadein':
 					// Display it
 					Spark(element).css({display: 'block', opacity: 0});
-				
+					
 					// Fade opacity to 100
 					Spark(element).animate({opacity: 1}, timeframe, easing, callback);
 					break;
-			
+				
 				case 'fadeout':
 					// Fade opacity to 0
 					Spark(element).animate({opacity: 0}, timeframe, easing, function() {
 						// Set opacity to 100
 						Spark(element).css({opacity: 1, display: 'none'});
-					
+						
 						// Run the callback
 						callback();
 					});
 					break;
-			
+				
 				case 'sneakin':
 					// Set overflow to hidden
 					Spark(element).css({overflow: 'hidden', display: 'block', opacity: 0});
-				
+					
 					// Get original height
 					original = Spark(element).attribute().offsetHeight;
-				
+					
 					// Set height to 0
 					Spark(element).css({height: 0});
-				
+					
 					// Slide height to original
 					Spark(element).animate({height: original, opacity: 1}, timeframe, easing, callback);
 					break;
-			
+				
 				case 'sneakout':
 					// Get original height
 					original = Spark(element).attribute().offsetHeight;
-				
+					
 					// Set overflow to hidden
 					Spark(element).css({overflow: 'hidden', height: original});
-				
+					
 					// Slide height to 0
 					Spark(element).animate({height: 0, opacity: 0}, timeframe, easing, function() {
 						// Set height to original
 						Spark(element).css({height: original + 'px', display: 'none'});
-					
+						
 						// Run the callback
 						callback();
 					});

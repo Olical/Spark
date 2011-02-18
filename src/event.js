a44568a49a06a10fb399d040393c1fbdadff1f9b
Oliver Caldwell <oliver@flowdev.co.uk>
2011-02-18 20:59:30 +0000

    Fixed the indenting in the event function

--- src/event.js
+++ src/event.js
@@ -10,25 +10,25 @@ SparkFn.event = function(type, callback) {
 		if(this.elements.hasOwnProperty(e)) {
 			// Grab the current element
 			element = this.elements[e];
-		
+			
 			// Set up the callback
 			runCallback = function(e) {
 				callback(Spark.fixEvent(e));
 			};
-		
+			
 			// Grab the previous reference
 			previousReference = this.data(element, 'Spark.event.' + type);
-		
+			
 			// Save the callback's reference for unsetting
 			this.data(element, 'Spark.event.' + type, runCallback);
-		
+			
 			// Check if the browser supports addEventListener or attachEvent and use it
 			if(element.addEventListener) {
 				// Removed the old event
 				if(previousReference) {
 					element.removeEventListener(type, previousReference, false);
 				}
-			
+				
 				// Assign event
 				element.addEventListener(type, runCallback, false);
 			}
@@ -37,7 +37,7 @@ SparkFn.event = function(type, callback) {
 				if(previousReference) {
 					element.detachEvent(type, previousReference);
 				}
-			
+				
 				// Assign event
 				element.attachEvent('on' + type, runCallback);
 			}

049278070f2a530bc7dca2f6fdd7b55ee835b4fc
Oliver Caldwell <oliver@flowdev.co.uk>
2011-02-19 19:26:52 +0000

    Took out the original fix, it was not working, and now I check if it is indeed IE before trying to fix stuff

--- src/css.js
+++ src/css.js
@@ -5,6 +5,7 @@ SparkFn.css = function(css) {
 	var calCos = null;
 	var radians = null;
 	var parentNode = null;
+	var browser = Spark.client().browser;
 	
 	// Loop through all of the elements
 	for(var e in this.elements) {
@@ -27,75 +28,30 @@ SparkFn.css = function(css) {
 				
 					// If opacity is being set we need to set all the other values for cross browser opacity
 					if(c == 'opacity') {
+						if(browser == 'Explorer') {
+							element.style.filter = 'alpha(opacity=' + (css[c] * 100) + ')';
+							element.style.zoom = '1';
+						}
+						
 						element.style.MozOpacity = css[c];
 						element.style.KhtmlOpacity = css[c];
-						element.style.filter = 'alpha(opacity=' + (css[c] * 100) + ')';
-						element.style.zoom = '1';
 					}
 					
 					// If rotation is being set we need to make it cross browser
 					if(c == 'rotation') {
-						if(Spark.client().browser == 'Explorer') {
+						if(browser == 'Explorer') {
 							radians = parseInt(css[c]) * (Math.PI * 2 / 360);
 							calSin = Math.sin(radians);
 							calCos = Math.cos(radians);
-						
-							if(!Spark(element.parentNode).classes('has', 'IETransformContainer')) {
-								parentNode = element.parentNode;
-								
-								// This is the container to offset the strange rotation behavior
-								var container = document.createElement('div');
-								Spark(container).classes('add', 'IETransformContainer');
-								
-								container.style.width = element.offsetWidth + 'px';
-								container.style.height = element.offsetHeight + 'px';
-								
-								container.xOriginalWidth = element.offsetWidth;
-								container.xOriginalHeight = element.offsetHeight;
-								container.style.position = 'absolute';
-								container.style.zIndex = element.currentStyle.zIndex;
-								
-								
-								var horizPaddingFactor = 0;
-								var vertPaddingFactor = 0;
-								if(element.currentStyle.display == 'block') {
-									container.style.left = element.offsetLeft + 13 - horizPaddingFactor + "px";
-									container.style.top = element.offsetTop + 13 +- vertPaddingFactor + 'px';
-								}
-								else {
-									container.style.left = element.offsetLeft + "px";
-									container.style.top = element.offsetTop + 'px';
-								}
-								
-								
-								element.style.top = "auto";
-								element.style.left = "auto"
-								element.style.bottom = "auto";
-								element.style.right = "auto";
-								// This is what we need in order to insert to keep the document
-								// flow ok
-								var replacement = element.cloneNode(true);
-								replacement.style.visibility = 'hidden';
-								
-								element.replaceNode(replacement);
-								
-								// now, wrap container around the original node ... 
-								element.style.position = 'absolute';
-								container.appendChild(element);
-								parentNode.insertBefore(container, replacement);
-								container.style.backgroundColor = 'transparent';
-								container.style.padding = '0';
-							}
+							element.style.filter = 'progid:DXImageTransform.Microsoft.Matrix(M11=' + calCos + ',M12=-' + calSin + ',M21=' + calSin + ',M22=' + calCos + ',sizingMethod="auto expand")';
+							element.style.zoom = '1';
 						}
 						
 						element.style.WebkitTransform = 'rotate(' + css[c] + ')';
 						element.style.MozTransform = 'rotate(' + css[c] + ')';
 						element.style.OTransform = 'rotate(' + css[c] + ')';
 						element.style.transform = 'rotate(' + css[c] + ')';
-						element.style.zoom = '1';
 					}
-					
-					element.style.filter = 'progid:DXImageTransform.Microsoft.Matrix(M11=' + calCos + ', M12=-' + calSin + ',M21=' + calSin + ', M22=' + calCos + ', sizingMethod="auto expand")';
 				}
 			}
 			else {

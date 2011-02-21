bc325f27944c7bfe5839eadb1b4a6199576b5add
Oliver Caldwell <oliver@flowdev.co.uk>
2011-02-21 15:36:07 +0000

    Made it so there is only one instance of returning this.

--- src/element.js
+++ src/element.js
@@ -9,32 +9,30 @@ SparkFn.add = function(method, tag, attributes, styles) {
 				this.elements[e].parentNode.removeChild(this.elements[e]);
 			}
 		}
-
-		// Return the Spark object to allow chaining
-		return this;
 	}
-	
-	// Create the new element
-	var construct = document.createElement(tag);
-	
-	// Make sure attributes is set
-	if(attributes) {
-		// Add attributes
-		Spark(construct).attribute(attributes);
-	}
-	
-	// Make sure styles is set
-	if(styles) {
-		// Add styles
-		Spark(construct).css(styles);
-	}
-	
-	// Loop through all elements
-	for(var e in this.elements) {
-		// Make sure that it is an element
-		if(this.elements.hasOwnProperty(e)) {
-			// Add the element
-			this.elements[e].appendChild(construct);
+	else {
+		// Create the new element
+		var construct = document.createElement(tag);
+		
+		// Make sure attributes is set
+		if(attributes) {
+			// Add attributes
+			Spark(construct).attribute(attributes);
+		}
+		
+		// Make sure styles is set
+		if(styles) {
+			// Add styles
+			Spark(construct).css(styles);
+		}
+		
+		// Loop through all elements
+		for(var e in this.elements) {
+			// Make sure that it is an element
+			if(this.elements.hasOwnProperty(e)) {
+				// Add the element
+				this.elements[e].appendChild(construct);
+			}
 		}
 	}
 	

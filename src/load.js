a190afcb0696b1c6152faacbdf3184de1fbbdf2d
Oliver Caldwell <oliver@flowdev.co.uk>
2011-02-21 17:26:57 +0000

    Made the load function use the element function rather than the add function which does not exist any more

--- src/load.js
+++ src/load.js
@@ -1,6 +1,6 @@
 SparkFn.load = function(file) {
 	// Create the script tag with the specified file as its src
-	Spark('head').add('script', {
+	Spark('head').element('insert', 'script', {
 		type: 'text/javascript',
 		src: file
 	});

17237462c18ea07f38bd027d39594f1bb9676af3
Oliver Caldwell <oliver@flowdev.co.uk>
2011-02-21 15:39:55 +0000

    Remade with the new element function and without the add or remove functions

--- spark-dev.js
+++ spark-dev.js
@@ -1049,40 +1049,52 @@ SparkFn.css = function(css) {
 	
 	// Return the Spark object
 	return this;
-};SparkFn.add = function(tag, attributes, styles) {
-	// Create the new element
-	var construct = document.createElement(tag);
-	
-	// Make sure attributes is set
-	if(attributes) {
-		// Add attributes
-		Spark(construct).attribute(attributes);
-	}
-	
-	// Make sure styles is set
-	if(styles) {
-		// Add styles
-		Spark(construct).css(styles);
-	}
-	
-	// Loop through all elements
-	for(var e in this.elements) {
-		// Make sure that it is an element
-		if(this.elements.hasOwnProperty(e)) {
-			// Add the element
-			this.elements[e].appendChild(construct);
+};SparkFn.element = function(method, tag, attributes, styles) {
+	// Check if we need to remove the element
+	if(method == 'remove') {
+		// Loop through all elements
+		for(var e in this.elements) {
+			// Make sure that it is an element
+			if(this.elements.hasOwnProperty(e)) {
+				// Remove the element
+				this.elements[e].parentNode.removeChild(this.elements[e]);
+			}
 		}
 	}
-	
-	// Return the Spark object to allow chaining
-	return this;
-};SparkFn.remove = function() {
-	// Loop through all elements
-	for(var e in this.elements) {
-		// Make sure that it is an element
-		if(this.elements.hasOwnProperty(e)) {
-			// Remove the element
-			this.elements[e].parentNode.removeChild(this.elements[e]);
+	else {
+		// Create the new element
+		var construct = document.createElement(tag);
+		
+		// Make sure attributes is set
+		if(attributes) {
+			// Add attributes
+			Spark(construct).attribute(attributes);
+		}
+		
+		// Make sure styles is set
+		if(styles) {
+			// Add styles
+			Spark(construct).css(styles);
+		}
+		
+		// Loop through all elements
+		for(var e in this.elements) {
+			// Make sure that it is an element
+			if(this.elements.hasOwnProperty(e)) {
+				// Perform the right action
+				if(method == 'prepend') {
+					// Prepend the element
+					this.elements[e].parentNode.insertBefore(construct, this.elements[e]);
+				}
+				else if(method == 'append') {
+					// Append the element
+					this.elements[e].parentNode.insertBefore(construct, this.elements[e].nextSibling);
+				}
+				else if(method == 'insert') {
+					// Insert the element
+					this.elements[e].appendChild(construct);
+				}
+			}
 		}
 	}
 	

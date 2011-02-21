d3388f94314cf140424f10aed56e9199fa8cb50a
Oliver Caldwell <oliver@flowdev.co.uk>
2011-02-21 15:34:54 +0000

    Removed remove.js and added that functionallity to the element function

--- src/element.js
+++ src/element.js
@@ -1,4 +1,19 @@
-SparkFn.add = function(tag, attributes, styles) {
+SparkFn.add = function(method, tag, attributes, styles) {
+	// Check if we need to remove the element
+	if(method == 'remove') {
+		// Loop through all elements
+		for(var e in this.elements) {
+			// Make sure that it is an element
+			if(this.elements.hasOwnProperty(e)) {
+				// Remove the element
+				this.elements[e].parentNode.removeChild(this.elements[e]);
+			}
+		}
+
+		// Return the Spark object to allow chaining
+		return this;
+	}
+	
 	// Create the new element
 	var construct = document.createElement(tag);
 	

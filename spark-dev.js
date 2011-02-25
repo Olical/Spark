f212cff1835558ca61cde3424a0f5e71985a3b44
Oliver Caldwell <oliver@flowdev.co.uk>
2011-02-25 20:30:10 +0000

    Remade with ready function changes

--- spark-dev.js
+++ spark-dev.js
@@ -169,31 +169,56 @@ window.SparkIn = function() {
 	return e;
 };SparkFn.ready = function(callback) {
 	// Set up variables
-	window.alreadyRunFlag = 0;
-	window.SparkRe = callback;
+	window.alreadyRunFlag = false;
 	
 	// Check if we can add an event listener
 	if(document.addEventListener) {
-		document.addEventListener("DOMContentLoaded", function() { alreadyRunFlag = 1; SparkRe() }, false);
+		// Add an event listener for for DOMContent loaded
+		Spark(document).event('DOMContentLoaded', function() {
+			// Set the flag to true
+			alreadyRunFlag = true;
+			
+			// Run the callback
+			callback();
+		});
 	}
 	else if(document.all && !window.opera) {
-		document.write('<script type="text/javascript" id="contentloadtag" defer="defer" src="javascript:void(0)"></script>');
+		// Create the script element
+		Spark('head').element('add', 'script', {
+			type: 'text/javascript',
+			id: 'contentloadtag',
+			defer: 'defer',
+			src: 'javascript:void(0)'
+		});
 		
-		var contentloadtag = document.getElementById('contentloadtag');
+		// Grab the element
+		var contentloadtag = Spark('#contentloadtag').attribute();
 		
+		// Add a listener for onReadyStateChange
 		contentloadtag.onreadystatechange = function() {
+			// If it is complete
 			if(this.readyState == 'complete') {
-				alreadyRunflag = 1;
-				SparkRe();
+				// Set the flag to true
+				alreadyRunflag = true;
+				
+				// Run the callback
+				callback();
 			}
 		};
 	}
 	
+	// Add a listener for onLoad
 	window.onload = function() {
-		setTimeout("if(!alreadyRunFlag) SparkRe()", 0);
+		// Set a timeout, this method is just the fallback
+		setTimeout(function() {
+			// Aslong as the flag is still false
+			if(alreadyRunFlag === false) {
+				// Run the callback
+				callback();
+			}
+		}, 0);
 	};
-};
-SparkFn.event = function(type, callback) {
+};SparkFn.event = function(type, callback) {
 	// Set up any variables
 	var element = null;
 	var runCallback = null;

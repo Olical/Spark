35246b507d75f4beb6e2edd88eb2e3afd8828230
Oliver Caldwell <oliver@flowdev.co.uk>
2011-02-25 20:28:39 +0000

    Added a lot of comments and implemented Spark to carry out methods such as creating elements in as many places as possible.

--- src/ready.js
+++ src/ready.js
@@ -4,24 +4,47 @@ SparkFn.ready = function(callback) {
 	
 	// Check if we can add an event listener
 	if(document.addEventListener) {
-		document.addEventListener("DOMContentLoaded", function() { alreadyRunFlag = true; callback() }, false);
+		// Add an event listener for for DOMContent loaded
+		Spark(document).event('DOMContentLoaded', function() {
+			// Set the flag to true
+			alreadyRunFlag = true;
+			
+			// Run the callback
+			callback();
+		});
 	}
 	else if(document.all && !window.opera) {
-		document.write('<script type="text/javascript" id="contentloadtag" defer="defer" src="javascript:void(0)"></script>');
+		// Create the script element
+		Spark('head').element('add', 'script', {
+			type: 'text/javascript',
+			id: 'contentloadtag',
+			defer: 'defer',
+			src: 'javascript:void(0)'
+		});
 		
-		var contentloadtag = document.getElementById('contentloadtag');
+		// Grab the element
+		var contentloadtag = Spark('#contentloadtag').attribute();
 		
+		// Add a listener for onReadyStateChange
 		contentloadtag.onreadystatechange = function() {
+			// If it is complete
 			if(this.readyState == 'complete') {
+				// Set the flag to true
 				alreadyRunflag = true;
+				
+				// Run the callback
 				callback();
 			}
 		};
 	}
 	
+	// Add a listener for onLoad
 	window.onload = function() {
+		// Set a timeout, this method is just the fallback
 		setTimeout(function() {
+			// Aslong as the flag is still false
 			if(alreadyRunFlag === false) {
+				// Run the callback
 				callback();
 			}
 		}, 0);

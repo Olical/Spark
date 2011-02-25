28502ec479a181ee5d0d892acd9907dfdf347454
Oliver Caldwell <oliver@flowdev.co.uk>
2011-02-25 20:39:46 +0000

    Added the contents to the trigger function which I have previously written and tested in jsFiddle

---
+++ src/trigger.js
@@ -0,0 +1,30 @@
+SparkFn.trigger = function(type) {
+	// Set up any variables
+	var element = null;
+	var trigger = null;
+	
+	// Loop through all elements
+	for(var e in this.elements) {
+		// Make sure that it is a property
+		if(this.elements.hasOwnProperty(e)) {
+			// Grab the element
+			element = this.elements[e];
+			
+			// Check for createEventObject
+			if(document.createEventObject){
+				// Trigger for Internet Explorer
+				trigger = document.createEventObject();
+				element.fireEvent('on' + type, trigger);
+			}
+			else {
+				// Trigger for the good browsers
+				trigger = document.createEvent('HTMLEvents');
+				trigger.initEvent(type, true, true);
+				element.dispatchEvent(trigger);
+			}
+		}
+	}
+	
+	// Return the Spark object to allow chaining
+	return this;
+};

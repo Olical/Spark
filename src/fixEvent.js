2373dd194517feff3877fcc8f584b2e6830aa21a
Golmote <yann.logan@live.fr>
2011-02-28 14:02:22 -0800

    Modified the comparisons to the undefined value and fixed the global declaration of offsetY.

--- src/fixEvent.js
+++ src/fixEvent.js
@@ -9,14 +9,14 @@ SparkFn.fixEvent = function(e) {
 	}
 	
 	// If its IE we need to copy srcElement over to target
-	if(e.target === undefined) {
+	if(typeof e.target === 'undefined') {
 		e.target = e.srcElement;
 	}
 	
 	// Fix the offsetX/Y in Firefox
-	var obj = e.target;
+	var obj = e.target, offsetX, offsetY;
 	if(obj.offsetParent && browser == 'Firefox') {
-		var offsetX = offsetY = 0;
+		offsetX = offsetY = 0;
 		
 		do {
 			offsetX += obj.offsetLeft;

4a63ff160ac4204666b3ba40f449c3d09ebd6dc8
Oliver Caldwell <oliver@flowdev.co.uk>
2011-03-03 13:37:16 +0000

    Fixed the indenting and merged the vars in the stop function

--- src/stop.js
+++ src/stop.js
@@ -1,7 +1,7 @@
 SparkFn.stop = function() {
 	// Set up any variables
-	var element = null;
-	var animations = null;
+	var element = null,
+		animations = null;
 	
 	// Loop through all of the elements
 	for(var e in this.elements) {
@@ -9,15 +9,15 @@ SparkFn.stop = function() {
 		if(this.elements.hasOwnProperty(e)) {
 			// Grab the current element
 			element = this.elements[e];
-		
+			
 			// Make sure it is set
 			if(this.data(element, 'Spark.animations') === undefined) {
 				this.data(element, 'Spark.animations', '');
 			}
-		
+			
 			// Get the animations
 			animations = this.data(element, 'Spark.animations').split(',');
-		
+			
 			// Loop through them all, canceling them all
 			for(var a in animations) {
 				clearTimeout(animations[a]);

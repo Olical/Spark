abdcc36809d63cd952d11a07dbac44d6e21243bc
Oliver Caldwell <oliver@flowdev.co.uk>
2011-03-03 13:19:56 +0000

    Merged all of the vriable initialisation into one var statement

--- src/animate.js
+++ src/animate.js
@@ -141,15 +141,15 @@ SparkFn.animate = function(properties, timeframe, easing, callback) {
 	var fps = 60;
 	
 	// Set up any required variables
-	var computed = null;
-	var original = null;
-	var difference = null;
-	var frames = null;
-	var change = null;
-	var unit = null;
-	var calculated = null;
-	var toSet = null;
-	var element = null;
+	var computed = null,
+		original = null,
+		difference = null,
+		frames = null,
+		change = null,
+		unit = null,
+		calculated = null,
+		toSet = null,
+		element = null;
 	
 	// Loop through all the elements
 	for(var e in this.elements) {

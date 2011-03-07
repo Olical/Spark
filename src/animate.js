a75080841784cf89bf400657e30313fd2c527478
Oliver Caldwell <oliver@flowdev.co.uk>
2011-03-07 21:01:11 +0000

    Cleaned up all of the transitions so they pass through jsLint cleanly

--- src/animate.js
+++ src/animate.js
@@ -7,7 +7,9 @@ SparkFn.animate = function(properties, timeframe, easing, callback) {
 			return -c *(t/=d)*(t-2) + b;
 		},
 		inOutQuad: function (t, b, c, d) {
-			if ((t/=d/2) < 1) return c/2*t*t + b;
+			if ((t/=d/2) < 1) {
+				return c/2*t*t + b;
+			}
 			return -c/2 * ((--t)*(t-2) - 1) + b;
 		},
 		inCubic: function (t, b, c, d) {
@@ -17,7 +19,9 @@ SparkFn.animate = function(properties, timeframe, easing, callback) {
 			return c*((t=t/d-1)*t*t + 1) + b;
 		},
 		inOutCubic: function (t, b, c, d) {
-			if ((t/=d/2) < 1) return c/2*t*t*t + b;
+			if ((t/=d/2) < 1) {
+				return c/2*t*t*t + b;
+			}
 			return c/2*((t-=2)*t*t + 2) + b;
 		},
 		inQuart: function (t, b, c, d) {
@@ -27,7 +31,9 @@ SparkFn.animate = function(properties, timeframe, easing, callback) {
 			return -c * ((t=t/d-1)*t*t*t - 1) + b;
 		},
 		inOutQuart: function (t, b, c, d) {
-			if ((t/=d/2) < 1) return c/2*t*t*t*t + b;
+			if ((t/=d/2) < 1) {
+				return c/2*t*t*t*t + b;
+			}
 			return -c/2 * ((t-=2)*t*t*t - 2) + b;
 		},
 		inQuint: function (t, b, c, d) {
@@ -37,7 +43,9 @@ SparkFn.animate = function(properties, timeframe, easing, callback) {
 			return c*((t=t/d-1)*t*t*t*t + 1) + b;
 		},
 		inOutQuint: function (t, b, c, d) {
-			if ((t/=d/2) < 1) return c/2*t*t*t*t*t + b;
+			if ((t/=d/2) < 1) {
+				return c/2*t*t*t*t*t + b;
+			}
 			return c/2*((t-=2)*t*t*t*t + 2) + b;
 		},
 		inSine: function (t, b, c, d) {
@@ -50,15 +58,21 @@ SparkFn.animate = function(properties, timeframe, easing, callback) {
 			return -c/2 * (Math.cos(Math.PI*t/d) - 1) + b;
 		},
 		inExpo: function (t, b, c, d) {
-			return (t==0) ? b : c * Math.pow(2, 10 * (t/d - 1)) + b;
+			return (t === 0) ? b : c * Math.pow(2, 10 * (t/d - 1)) + b;
 		},
 		outExpo: function (t, b, c, d) {
-			return (t==d) ? b+c : c * (-Math.pow(2, -10 * t/d) + 1) + b;
+			return (t === d) ? b+c : c * (-Math.pow(2, -10 * t/d) + 1) + b;
 		},
 		inOutExpo: function (t, b, c, d) {
-			if (t==0) return b;
-			if (t==d) return b+c;
-			if ((t/=d/2) < 1) return c/2 * Math.pow(2, 10 * (t - 1)) + b;
+			if(t === 0) {
+				return b;
+			}
+			else if(t === d) {
+				return b + c;
+			}
+			else if((t/=d/2) < 1) {
+				return c/2 * Math.pow(2, 10 * (t - 1)) + b;
+			}
 			return c/2 * (-Math.pow(2, -10 * --t) + 2) + b;
 		},
 		inCirc: function (t, b, c, d) {
@@ -68,57 +82,119 @@ SparkFn.animate = function(properties, timeframe, easing, callback) {
 			return c * Math.sqrt(1 - (t=t/d-1)*t) + b;
 		},
 		inOutCirc: function (t, b, c, d) {
-			if ((t/=d/2) < 1) return -c/2 * (Math.sqrt(1 - t*t) - 1) + b;
+			if ((t/=d/2) < 1) {
+				return -c/2 * (Math.sqrt(1 - t*t) - 1) + b;
+			}
 			return c/2 * (Math.sqrt(1 - (t-=2)*t) + 1) + b;
 		},
 		inElastic: function (t, b, c, d, a, p) {
-			if (t==0) { return b; } 
-			if ((t/=d)==1) { return b+c; }
-			if (!p) { p=d*.3; }
-			if (a < Math.abs(c)) {  a=c; s=p/4; }
-			else { a=Math.abs(c); s: p/(2*Math.PI) * Math.asin(c/a);}
+			if(t === 0) {
+				return b;
+			} 
+			else if((t/=d) === 1) {
+				return b+c;
+			}
+			if(!p) {
+				p=d*0.3;
+			}
+			if(a < Math.abs(c)) {
+				a=c;
+				s=p/4;
+			}
+			else {
+				a = Math.abs(c);
+				s = p/(2*Math.PI) * Math.asin(c/a);
+			}
 			return -(a*Math.pow(2,10*(t-=1)) * Math.sin( (t*d-s)*(2*Math.PI)/p )) + b;
 		},
 		outElastic: function (t, b, c, d, a, p) {
-			if (t==0) return b;  if ((t/=d)==1) return b+c;  if (!p) p=d*.3;
-			if (a < Math.abs(c)) { a=c; var s=p/4; }
-			else {   a=Math.abs(c); var s= p/(2*Math.PI) * Math.asin (c/a);}
+			var s = null;
+			if(t === 0) {
+				return b;
+			}
+			else if((t/=d) === 1) {
+				return b+c;
+			}
+			else if (!p) {
+				p=d*0.3;
+			}
+			if(a < Math.abs(c)) {
+				a=c;
+				s=p/4;
+			}
+			else {
+				a=Math.abs(c);
+				s= p/(2*Math.PI) * Math.asin (c/a);
+			}
 			return a*Math.pow(2,-10*t) * Math.sin( (t*d-s)*(2*Math.PI)/p ) + c + b;
 		},
 		inOutElastic: function (t, b, c, d, a, p) {
-			if (t==0) return b;  
-			if ((t/=d/2)==2) return b+c;  
-			if (!p) p=d*(.3*1.5);
-			if (a < Math.abs(c)) { a=c; var s=p/4; }
-			else {a=Math.abs(c);var s= p/(2*Math.PI) * Math.asin (c/a);}
-			if (t < 1) {return -.5*(a*Math.pow(2,10*(t-=1)) * Math.sin( (t*d-s)*(2*Math.PI)/p )) + b;}
-			return a*Math.pow(2,-10*(t-=1)) * Math.sin( (t*d-s)*(2*Math.PI)/p )*.5 + c + b;
+			var s = null;
+			if (t === 0) {
+				return b;
+			}
+			else if ((t/=d/2) === 2) {
+				return b+c;
+			}
+			else if (!p) {
+				p=d*(0.3*1.5);
+			}
+			if (a < Math.abs(c)) {
+				a=c;
+				s=p/4;
+			}
+			else {
+				a=Math.abs(c);
+				s= p/(2*Math.PI) * Math.asin (c/a);
+			}
+			if (t < 1) {
+				return -0.5*(a*Math.pow(2,10*(t-=1)) * Math.sin( (t*d-s)*(2*Math.PI)/p )) + b;
+			}
+			return a*Math.pow(2,-10*(t-=1)) * Math.sin( (t*d-s)*(2*Math.PI)/p )*0.5 + c + b;
 		},
 		inBack: function (t, b, c, d, s) {
-			if (s== undefined) s: 1.70158;
+			if(typeof s === 'undefined') {
+				s = 1.70158;
+			}
 			return c*(t/=d)*t*((s+1)*t - s) + b;
 		},
 		outBack: function (t, b, c, d, s) {
-			if (s== undefined) s: 1.70158;
+			if (typeof s === 'undefined') {
+				s = 1.70158;
+			}
 			return c*((t=t/d-1)*t*((s+1)*t + s) + 1) + b;
 		},
 		inOutBack: function (t, b, c, d, s) {
-			if (s== undefined) s: 1.70158; 
-			if ((t/=d/2) < 1) return c/2*(t*t*(((s*=(1.525))+1)*t - s)) + b;
+			if (typeof s === 'undefined') {
+				s = 1.70158;
+			}
+			if ((t/=d/2) < 1) {
+				return c/2*(t*t*(((s*=(1.525))+1)*t - s)) + b;
+			}
 			return c/2*((t-=2)*t*(((s*=(1.525))+1)*t + s) + 2) + b;
 		},
 		inBounce: function (t, b, c, d) {
 			return c - this.outBounce (d-t, 0, c, d) + b;
 		},
 		outBounce: function (t, b, c, d) {
-			if ((t/=d) < (1/2.75)) { return c*(7.5625*t*t) + b;} 
-			else if (t < (2/2.75)) { return c*(7.5625*(t-=(1.5/2.75))*t + .75) + b;} 
-			else if (t < (2.5/2.75)) { return c*(7.5625*(t-=(2.25/2.75))*t + .9375) + b;}
-			else { return c*(7.5625*(t-=(2.625/2.75))*t + .984375) + b; }
+			if ((t/=d) < (1/2.75)) {
+				return c*(7.5625*t*t) + b;
+			} 
+			else if (t < (2/2.75)) {
+				return c*(7.5625*(t-=(1.5/2.75))*t + 0.75) + b;
+			} 
+			else if (t < (2.5/2.75)) {
+				return c*(7.5625*(t-=(2.25/2.75))*t + 0.9375) + b;
+			}
+			else {
+				return c*(7.5625*(t-=(2.625/2.75))*t + 0.984375) + b;
+			}
 		},
 		inOutBounce: function (t, b, c, d) {
-			if (t < d/2) return this.inBounce (t*2, 0, c, d) * .5 + b;
-			return this.outBounce (t*2-d, 0, c, d) * .5 + c*.5 + b;
+			if (t < d/2) {
+				return this.inBounce (t*2, 0, c, d) * 0.5 + b;
+			}
+			return this.outBounce (t*2-d, 0, c, d) * 0.5 + c*0.5 + b;
 		}
 	};
 	

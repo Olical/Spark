ab9cba11c2ede18a1f94374f94a73b682ad101b8
Oliver Caldwell <oliver@flowdev.co.uk>
2011-03-07 21:44:43 +0000

    Implemented strict comparisons

--- src/json.js
+++ src/json.js
@@ -1,9 +1,9 @@
 SparkFn.json = function(method, data) {
 	// Decode or encode depending on the method string
-	if(method == 'encode') {
+	if(method === 'encode') {
 		return JSON.stringify(data);
 	}
-	else if(method == 'decode') {
+	else if(method === 'decode') {
 		return JSON.parse(data);
 	}
 };

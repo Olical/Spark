906712627db30e216d8ef117e939357c0215c2f8
Oliver Caldwell <oliver@flowdev.co.uk>
2011-04-02 11:15:03 +0100

    Made the running of the callback in the toggle function more graceful.

--- src/toggle.js
+++ src/toggle.js
@@ -52,9 +52,6 @@ SparkFn.toggle = function(method, timeframe, easing, callback) {
 			if(method === 'visibility') {
 				// Toggle transition with either show or hide
 				Spark(element).transition(((element.style.display === 'none') ? 'show' : 'hide'), timeframe, easing);
-				
-				// Set the timeframe to 0
-				timeframe = 0;
 			}
 			else {
 				// Toggle transition with the calculated method
@@ -63,8 +60,15 @@ SparkFn.toggle = function(method, timeframe, easing, callback) {
 		}
 	}
 	
-	// Set the callback to be run
-	setTimeout(callback, timeframe);
+	// Check how the callback needs to be run
+	if(method === 'visibility') {
+		// Run the callback
+		callback();
+	}
+	else {
+		// Set the callback to be run
+		setTimeout(callback, timeframe);
+	}
 	
 	// Set up the offset for chaining
 	this.offset += timeframe;
